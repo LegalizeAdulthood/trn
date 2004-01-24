7eb31ebdd25fce808d975e384f96d213177940fb
wayned <wayned@localhost>
2004-01-24 17:59:21 +0000

    Updated prototypes.

--- nntpinit.h
+++ nntpinit.h
@@ -6,6 +6,7 @@
 int init_nntp _((void));
 int server_init _((char*));
 void cleanup_nntp _((void));
+int get_tcp_socket _((char*,int,char*));
 #ifdef NONETDB
 unsigned long inet_addr _((char*));
 #endif
