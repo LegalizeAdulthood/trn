e8ca3a246ce910891f55cb32f530333f0c083a82
Richard <legalize@xmission.com>
2023-03-26 07:04:15 -0600

    Test interpolate article name inside remote newsgroup article closed

--- tests/test_interp.cpp
+++ tests/test_interp.cpp
@@ -3,12 +3,17 @@
 #include <array>
 
 #include "common.h"
+#include "datasrc.h"
+#include "env.h"
 #include "intrp.h"
 #include "ng.h"
 
 constexpr int BUFFER_SIZE{4096};
 
-struct InterpolatorTest : public testing::Test
+namespace
+{
+
+struct InterpolatorTest : testing::Test
 {
 protected:
     void SetUp() override
@@ -19,47 +24,59 @@ protected:
         g_art = 0;
     }
 
-    std::array<char, BUFFER_SIZE> buffer{};
+    void TearDown() override
+    {
+        datasrc_finalize();
+    }
+
+    char *interpolate(char *pattern, const char *stoppers = "")
+    {
+        return dointerp(m_buffer.data(), BUFFER_SIZE, pattern, stoppers, nullptr);
+    }
+
+    std::array<char, BUFFER_SIZE> m_buffer{};
 };
+
+} // namespace
 
 TEST_F(InterpolatorTest, noEscapes)
 {
     char pattern[]{"this string contains no escapes"};
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "", nullptr);
+    const char *new_pattern = interpolate(pattern);
 
     ASSERT_EQ('\0', *new_pattern);
-    ASSERT_EQ(std::string{pattern}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{pattern}, std::string{m_buffer.data()});
 }
 
 TEST_F(InterpolatorTest, firstStopCharacter)
 {
     char pattern[]{"this string contains no escapes [but contains stop characters]"};
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "[]", nullptr);
+    const char *new_pattern = interpolate(pattern, "[]");
 
     ASSERT_EQ('[', *new_pattern);
-    ASSERT_EQ(std::string{"this string contains no escapes "}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{"this string contains no escapes "}, std::string{m_buffer.data()});
 }
 
 TEST_F(InterpolatorTest, subsequentStopCharacter)
 {
     char pattern[]{"this string contains no escapes [but contains stop characters]"};
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "()[]", nullptr);
+    const char *new_pattern = interpolate(pattern, "()[]");
 
     ASSERT_EQ('[', *new_pattern);
-    ASSERT_EQ(std::string{"this string contains no escapes "}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{"this string contains no escapes "}, std::string{m_buffer.data()});
 }
 
 TEST_F(InterpolatorTest, articleNumberOutsideNewsgroup)
 {
     char pattern[]{"Article %a"};
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "", nullptr);
+    const char *new_pattern = interpolate(pattern);
 
     ASSERT_EQ('\0', *new_pattern);
-    ASSERT_EQ(std::string{"Article "}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{"Article "}, std::string{m_buffer.data()});
 }
 
 TEST_F(InterpolatorTest, articleNumberInsideNewsgroup)
@@ -68,18 +85,33 @@ TEST_F(InterpolatorTest, articleNumberInsideNewsgroup)
     g_in_ng = true;
     g_art = 624;
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "", nullptr);
+    const char *new_pattern = interpolate(pattern);
 
     ASSERT_EQ('\0', *new_pattern);
-    ASSERT_EQ(std::string{"Article 624"}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{"Article 624"}, std::string{m_buffer.data()});
 }
 
 TEST_F(InterpolatorTest, articleNameOutsideNewsgroup)
 {
     char pattern[]{"Article %A"};
 
-    const char *new_pattern = dointerp(buffer.data(), BUFFER_SIZE, pattern, "", nullptr);
+    const char *new_pattern = interpolate(pattern);
 
     ASSERT_EQ('\0', *new_pattern);
-    ASSERT_EQ(std::string{"Article "}, std::string{buffer.data()});
+    ASSERT_EQ(std::string{"Article "}, std::string{m_buffer.data()});
+}
+
+TEST_F(InterpolatorTest, articleNameInsideRemoteNewsgroupArticleClosed)
+{
+    char pattern[]{"Article %A"};
+    g_in_ng = true;
+    g_art = 624;
+    g_tmp_dir = getenv("TEMP");
+    datasrc_init();
+    g_datasrc = datasrc_first();
+
+    const char *new_pattern = interpolate(pattern);
+
+    ASSERT_EQ('\0', *new_pattern);
+    ASSERT_EQ(std::string{"Article "}, std::string{m_buffer.data()});
 }
