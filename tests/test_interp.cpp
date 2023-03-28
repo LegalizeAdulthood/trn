a020edf5dd62ea8f03a2974d258d1546647eb4be
Richard <legalize@xmission.com>
2023-03-27 21:33:25 -0600

    Improve setup/teardown for dointerp tests

--- tests/test_interp.cpp
+++ tests/test_interp.cpp
@@ -2,11 +2,16 @@
 
 #include <array>
 
-#include "common.h"
-#include "datasrc.h"
-#include "env.h"
-#include "intrp.h"
-#include "ng.h"
+#include <common.h>
+
+#include <datasrc.h>
+#include <env-internal.h>
+#include <init.h>
+#include <intrp.h>
+#include <ng.h>
+#include <ngdata.h>
+#include <util.h>
+#include <util2.h>
 
 constexpr int BUFFER_SIZE{4096};
 
@@ -20,13 +25,53 @@ protected:
     {
         Test::SetUp();
 
-        g_in_ng = false;
-        g_art = 0;
+        const char *const vars[] = {"HOME=",     "LOGDIR=",    "TMPDIR=",   "TMP=",    "USER=",  "LOGNAME=",
+                                    "USERNAME=", "HOMEDRIVE=", "HOMEPATH=", "DOTDIR=", "TRNDIR="};
+        for (const char *name : vars)
+        {
+            putenv(name);
+        }
+        env_init(
+            m_tcbuf.data(), true,
+            [](char *)
+            {
+                g_login_name = savestr("bonzo");
+                g_real_name = savestr("Bonzo the Monkey");
+                return true;
+            },
+            [](char *)
+            {
+                g_local_host = savestr("localhost");
+                g_p_host_name = savestr("news.gmane.io");
+                return true;
+            });
+        datasrc_init();
+        g_datasrc = datasrc_first();
     }
 
     void TearDown() override
     {
+        g_lastart = 0;
+        g_art = 0;
+        g_in_ng = false;
+        g_datasrc = nullptr;
         datasrc_finalize();
+        g_datasrc_cnt = 0;
+
+        safefree0(g_datasrc_list);
+        safefree0(g_nntp_auth_file);
+        safefree0(g_home_dir);
+        safefree0(g_real_name);
+        g_tmp_dir = nullptr;
+        safefree0(g_login_name);
+        safefree0(g_real_name);
+        safefree0(g_local_host);
+        safefree0(g_p_host_name);
+        g_dot_dir = nullptr;
+        safefree0(g_trn_dir);
+        safefree0(g_lib);
+
+        Test::TearDown();
     }
 
     char *interpolate(char *pattern, const char *stoppers = "")
@@ -34,6 +79,7 @@ protected:
         return dointerp(m_buffer.data(), BUFFER_SIZE, pattern, stoppers, nullptr);
     }
 
+    std::array<char, TCBUF_SIZE> m_tcbuf{};
     std::array<char, BUFFER_SIZE> m_buffer{};
 };
 
@@ -105,10 +151,8 @@ TEST_F(InterpolatorTest, articleNameInsideRemoteNewsgroupArticleClosed)
 {
     char pattern[]{"Article %A"};
     g_in_ng = true;
+    g_lastart = 623;
     g_art = 624;
-    g_tmp_dir = getenv("TEMP");
-    datasrc_init();
-    g_datasrc = datasrc_first();
 
     const char *new_pattern = interpolate(pattern);
 
