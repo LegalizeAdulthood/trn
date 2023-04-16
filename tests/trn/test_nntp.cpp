34ce04e9ae7490aa17c369634342d6d0f424d4ca
Richard <legalize@xmission.com>
2023-04-16 11:29:35 -0600

    Test server_init when the connection fails

--- tests/trn/test_nntp.cpp
+++ tests/trn/test_nntp.cpp
@@ -4,6 +4,8 @@
 #include <nntpclient.h>
 #include <nntpinit.h>
 
+#include <utility>
+
 using namespace testing;
 
 class MockNNTPConnection : public INNTPConnection
@@ -32,21 +34,48 @@ public:
     MOCK_METHOD(ConnectionPtr, create, (const char *machine, int port, const char *service), (override));
 };
 
-TEST(NNTPTest, server_init_ok)
+class NNTPTest : public Test
 {
-    const char *const machine{"news.gmane.io"};
-    init_nntp();
-    StrictMock<MockNNTPConnectionFactory> factory;
-    set_nntp_connection_factory([&factory](const char *machine, int port, const char *service)
-                                { return factory.create(machine, port, service); });
-    std::shared_ptr<StrictMock<MockNNTPConnection>> connection = std::make_shared<StrictMock<MockNNTPConnection>>();
-    EXPECT_CALL(factory, create(StrEq(machine), _, StrEq("nntp"))).WillOnce(Return(connection));
-    EXPECT_CALL(*connection, read_line(_))
+public:
+    ~NNTPTest() override = default;
+
+protected:
+    void SetUp() override
+    {
+        init_nntp();
+        set_nntp_connection_factory([this](const char *machine, int port, const char *service)
+                                    { return m_factory.create(machine, port, service); });
+        m_connection = std::make_shared<StrictMock<MockNNTPConnection>>();
+    }
+
+    void configure_factory_create(ConnectionPtr result)
+    {
+        EXPECT_CALL(m_factory, create(StrEq(m_machine), _, StrEq("nntp"))).WillOnce(Return(std::move(result)));
+    }
+
+    const char *const                               m_machine{"news.gmane.io"};
+    StrictMock<MockNNTPConnectionFactory>           m_factory;
+    std::shared_ptr<StrictMock<MockNNTPConnection>> m_connection;
+};
+
+TEST_F(NNTPTest, server_init_ok)
+{
+    configure_factory_create(m_connection);
+    EXPECT_CALL(*m_connection, read_line(_))
         .WillOnce(Return("200 news.gmane.io InterNetNews NNRP server INN 2.6.3 ready (posting ok)"))
         .WillOnce(Return("200 news.gmane.io InterNetNews NNRP server INN 2.6.3 ready (posting ok)"));
-    EXPECT_CALL(*connection, write_line(StrEq("MODE READER"), _)).Times(1);
+    EXPECT_CALL(*m_connection, write_line(StrEq("MODE READER"), _)).Times(1);
 
-    const int result = server_init(machine);
+    const int result = server_init(m_machine);
 
     EXPECT_EQ(NNTP_POSTOK_VAL, result);
+}
+
+TEST_F(NNTPTest, server_init_connection_failed)
+{
+    configure_factory_create(nullptr);
+
+    const int result = server_init(m_machine);
+
+    EXPECT_EQ(-1, result);
 }
