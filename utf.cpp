0320946267328be686a313b2ec7d008864a09d3d
Richard <legalize@xmission.com>
2023-04-18 20:47:30 -0600

    Use nullptr; suppress clang-format of table data

--- utf.cpp
+++ utf.cpp
@@ -30,6 +30,7 @@
 #define XXXXXX INVALID_CODE_POINT
 
 static const CODE_POINT s_cp1252_himap[128] = {
+    // clang-format off
     0x20ac, XXXXXX, 0x201a, 0x0192, 0x201e, 0x2026, 0x2020, 0x2021,	0x02c6, 0x2030, 0x0160, 0x2039, 0x0152, XXXXXX, 0x017d, XXXXXX,
     XXXXXX, 0x2018, 0x2019, 0x201c, 0x201d, 0x00b7, 0x2013, 0x2014,	0x02dc, 0x2122, 0x0161, 0x203a, 0x0153, XXXXXX, 0x017e, 0x0178,
     0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x00a4, 0x00a5, 0x00a6, 0x00a7,	0x00a8, 0x00a9, 0x00aa, 0x00ab, 0x00ac, XXXXXX, 0x00ae, 0x00af,
@@ -38,9 +39,11 @@ static const CODE_POINT s_cp1252_himap[128] = {
     0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,	0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
     0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,	0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
     0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,	0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff,
+    // clang-format on
 };
 
 static const CODE_POINT s_iso8859_1_himap[128] = {
+    // clang-format off
     XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,	XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
     XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,	XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
     0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x00a4, 0x00a5, 0x00a6, 0x00a7,	0x00a8, 0x00a9, 0x00aa, 0x00ab, 0x00ac, XXXXXX, 0x00ae, 0x00af,
@@ -49,9 +52,11 @@ static const CODE_POINT s_iso8859_1_himap[128] = {
     0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,	0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
     0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,	0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
     0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,	0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff,
+    // clang-format on
 };
 
 static const CODE_POINT s_iso8859_15_himap[128] = {
+    // clang-format off
     XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,	XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
     XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,	XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX, XXXXXX,
     0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x20ac, 0x00a5, 0x0160, 0x00a7,	0x0161, 0x00a9, 0x00aa, 0x00ab, 0x00ac, XXXXXX, 0x00ae, 0x00af,
@@ -60,6 +65,7 @@ static const CODE_POINT s_iso8859_15_himap[128] = {
     0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,	0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
     0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,	0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
     0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,	0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff,
+    // clang-format on
 };
 
 struct CHARSET_DESC
@@ -71,13 +77,14 @@ struct CHARSET_DESC
 
 static const CHARSET_DESC s_charset_descs[] = {
     /* Tags defined in utf.h go first; these are short labels for charsubst.c */
-    { TAG_ASCII, CHARSET_ASCII, NULL },
-    { "us-ascii", CHARSET_ASCII, NULL },
-    { "ascii", CHARSET_ASCII, NULL },
+    // clang-format off
+    { TAG_ASCII, CHARSET_ASCII, nullptr },
+    { "us-ascii", CHARSET_ASCII, nullptr },
+    { "ascii", CHARSET_ASCII, nullptr },
 
-    { TAG_UTF8, CHARSET_UTF8, NULL },
-    { "utf-8", CHARSET_UTF8, NULL },
-    { "utf8", CHARSET_UTF8, NULL },
+    { TAG_UTF8, CHARSET_UTF8, nullptr },
+    { "utf-8", CHARSET_UTF8, nullptr },
+    { "utf8", CHARSET_UTF8, nullptr },
 
     { TAG_ISO8859_1, CHARSET_ISO8859_1, s_iso8859_1_himap },
     { "iso8859-1", CHARSET_ISO8859_1, s_iso8859_1_himap },
@@ -91,7 +98,8 @@ static const CHARSET_DESC s_charset_descs[] = {
     { "cp1252", CHARSET_WINDOWS_1252, s_cp1252_himap },
     { "windows-1252", CHARSET_WINDOWS_1252, s_cp1252_himap },
 
-    { NULL, CHARSET_UNKNOWN, nullptr }
+    { nullptr, CHARSET_UNKNOWN, nullptr }
+    // clang-format on
 };
 
 struct gstate {
@@ -101,7 +109,7 @@ struct gstate {
     const CODE_POINT* himap_out;
 };
 
-static gstate s_gs = { CHARSET_UTF8, CHARSET_UTF8, NULL, NULL };
+static gstate s_gs = { CHARSET_UTF8, CHARSET_UTF8, nullptr, nullptr };
 
 
 static int find_charset(const char *s)
@@ -112,7 +120,7 @@ static int find_charset(const char *s)
 	for (i = 0; ; i += 1) {
 	    const char *name = s_charset_descs[i].name;
 	    int j;
-	if (name == NULL) break;
+	if (name == nullptr) break;
 	    for (j = 0; ; j++) {
 	if (tolower(s[j]) != name[j]) break;
 		if (s[j] == 0 && name[j] == 0)
@@ -127,14 +135,14 @@ static int find_charset(const char *s)
 
 static const CHARSET_DESC *find_charset_desc(int id)
 {
-    const CHARSET_DESC *it = NULL;
+    const CHARSET_DESC *it = nullptr;
     int i;
     for (i = 0; ; i += 1) {
 	const CHARSET_DESC *node = &s_charset_descs[i];
-    if (node->name == NULL) break;
+    if (node->name == nullptr) break;
 	if (id == node->id)
 	    it = node;
-    if (it != NULL) break;
+    if (it != nullptr) break;
     }
     return it;
 }
@@ -170,7 +178,7 @@ const char *output_charset_name()
 
 int byte_length_at(const char *s)
 {
-    int it = s != NULL; /* correct for ASCII */
+    int it = s != nullptr; /* correct for ASCII */
     if (!it) {
 	;
     } else if (IS_UTF8(s_gs.in)) {
@@ -263,7 +271,7 @@ int visual_length_between(const char *s1, const char *s2)
 CODE_POINT code_point_at(const char *s)
 {
     CODE_POINT it;
-    if (s != NULL) {
+    if (s != nullptr) {
 	if (IS_UTF8(s_gs.in)) {
 	    size_t n = strlen(s);
 	    if (n > 0 && (*s & 0x80) == 0) {
@@ -283,7 +291,7 @@ CODE_POINT code_point_at(const char *s)
 	    }
 	} else if (s_gs.in == CHARSET_ASCII) {
 	    it = *s & 0x7F;
-	} else if (s_gs.himap_in != NULL) {
+	} else if (s_gs.himap_in != nullptr) {
 	    it = *s & 0xFF; /* I hate signed/unsigned conversions */
 	    if (it & 0x80)
 		it = s_gs.himap_in[it & 0x7F];
@@ -298,7 +306,7 @@ static int insert_utf8_at(char *s, CODE_POINT c)
 {
     int it;
     /* FIXME - should we check if s has enough space? */
-    if (s == NULL)
+    if (s == nullptr)
 	it = 0;
     else if (c <= 0x0000007F) {
 	s[0] = (char)c;
@@ -346,14 +354,14 @@ int insert_unicode_at(char *s, CODE_POINT c)
 {
     int it;
     /* FIXME - should we check if s has enough space? */
-    if (s == NULL)
+    if (s == nullptr)
 	it = 0;
     else if (IS_UTF8(s_gs.in)) {
 	it = insert_utf8_at(s, c);
     } else if (c <= 0x7F) {
 	s[0] = (char)c;
 	it = 1;
-    } else if (s_gs.himap_in != NULL) {
+    } else if (s_gs.himap_in != nullptr) {
 	it = insert_utf8_at(s, s_gs.himap_in[c & 0x7F]);
     }
     return it;
@@ -361,7 +369,7 @@ int insert_unicode_at(char *s, CODE_POINT c)
 
 bool at_norm_char(const char *s)
 {
-    int it = s != NULL;
+    int it = s != nullptr;
     if (it) { it = *s != 0; }
     if (it) {
 	if (s_gs.in == CHARSET_UTF8) {
@@ -370,7 +378,7 @@ bool at_norm_char(const char *s)
 	}
 	else if (U(*s) < 0x80)
 	    it = (U(*s) >= ' ' && U(*s) < 0x7F);
-	else if (s_gs.himap_in != NULL)
+	else if (s_gs.himap_in != nullptr)
 	    it = s_gs.himap_in[U(*s) & 0x7F] != INVALID_CODE_POINT;
     }
     return it;
@@ -379,7 +387,7 @@ bool at_norm_char(const char *s)
 int put_char_adv(char **strptr, bool outputok)
 {
     int it;
-    if (strptr == NULL) {
+    if (strptr == nullptr) {
 	it = 0;
     } else {
 	char *s = *strptr;
@@ -410,7 +418,7 @@ int put_char_adv(char **strptr, bool outputok)
 char *create_utf8_copy(char *s)
 {
     char *it = s;
-    if (s != NULL) {
+    if (s != nullptr) {
 	int slen;
 	int tlen;
 	int i;
