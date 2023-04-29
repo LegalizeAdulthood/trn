179ffc8ab14ca8f19ea8698c05c269eccb1fb49d
Richard <legalize@xmission.com>
2023-04-29 11:21:39 -0600

    Localize loop variables in mempool

--- mempool.cpp
+++ mempool.cpp
@@ -51,8 +51,8 @@ void mp_init()
     s_mpfrags[i-1].next = 0;
     s_mp_first_free_frag = 1;	/* first free fragment */
 
-    for (int i = 0; i < MAX_MEM_POOLS; i++)
-	s_mpheads[i].current = 0;
+    for (int j = 0; j < MAX_MEM_POOLS; j++)
+	s_mpheads[j].current = 0;
 }
 
 /* returns the fragment number */
