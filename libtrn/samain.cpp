aeb5b50cf81d676b8a8b589c57905f669f0632c9
Richard <legalize@xmission.com>
2025-01-24 19:25:27 -0700

    Expand tabs in samain.cpp

--- libtrn/samain.cpp
+++ libtrn/samain.cpp
@@ -32,24 +32,24 @@ void sa_init()
 {
     sa_init_context();
     if (g_lastart == 0 || g_absfirst > g_lastart)
-	return;		/* no articles */
-    if (s_initscreen())		/* If not able to init screen...*/
-	return;				/* ...most likely dumb terminal */
-    sa_initmode();			/*      mode differences */
+        return;         /* no articles */
+    if (s_initscreen())         /* If not able to init screen...*/
+        return;                         /* ...most likely dumb terminal */
+    sa_initmode();                      /*      mode differences */
     sa_init_threads();
     g_sa_mode_read_elig = false;
-    if (g_firstart > g_lastart)		/* none unread */
-	g_sa_mode_read_elig = true;	/* unread+read in some situations */
-    if (!sa_initarts())			/* init article array(s) */
-	return;				/* ... no articles */
+    if (g_firstart > g_lastart)         /* none unread */
+        g_sa_mode_read_elig = true;     /* unread+read in some situations */
+    if (!sa_initarts())                 /* init article array(s) */
+        return;                         /* ... no articles */
 #ifdef PENDING
     if (g_sa_mode_read_elig) {
-	g_sc_fill_read = true;
-	g_sc_fill_max = g_absfirst - 1;
+        g_sc_fill_read = true;
+        g_sc_fill_max = g_absfirst - 1;
     }
 #endif
     s_save_context();
-    g_sa_initialized = true;		/* all went well... */
+    g_sa_initialized = true;            /* all went well... */
 }
 
 void sa_init_ents()
@@ -65,21 +65,21 @@ void sa_clean_ents()
 }
 
 /* returns entry number that was added */
-//ART_NUM artnum;		/* article number to be added */
+//ART_NUM artnum;               /* article number to be added */
 long sa_add_ent(ART_NUM artnum)
 {
     g_sa_num_ents++;
     if (g_sa_num_ents > s_sa_ents_alloc) {
-	s_sa_ents_alloc += 100;
-	if (s_sa_ents_alloc == 100) {	/* newly allocated */
-	    /* don't use number 0, just allocate it and skip it */
-	    g_sa_num_ents = 2;
-	    g_sa_ents = (SA_ENTRYDATA*)safemalloc(s_sa_ents_alloc
-					* sizeof (SA_ENTRYDATA));
+        s_sa_ents_alloc += 100;
+        if (s_sa_ents_alloc == 100) {   /* newly allocated */
+            /* don't use number 0, just allocate it and skip it */
+            g_sa_num_ents = 2;
+            g_sa_ents = (SA_ENTRYDATA*)safemalloc(s_sa_ents_alloc
+                                        * sizeof (SA_ENTRYDATA));
         } else {
-	    g_sa_ents = (SA_ENTRYDATA*)saferealloc((char*)g_sa_ents,
-			s_sa_ents_alloc * sizeof (SA_ENTRYDATA));
-	}
+            g_sa_ents = (SA_ENTRYDATA*)saferealloc((char*)g_sa_ents,
+                        s_sa_ents_alloc * sizeof (SA_ENTRYDATA));
+        }
     }
     long cur = g_sa_num_ents - 1;
     g_sa_ents[cur].artnum = artnum;
@@ -93,30 +93,30 @@ void sa_cleanmain()
 {
     sa_clean_ents();
 
-    g_sa_mode_zoom = false;	/* doesn't survive across groups */
+    g_sa_mode_zoom = false;     /* doesn't survive across groups */
     /* remove the now-unused scan-context */
     s_delete_context(g_sa_scan_context);
     s_sa_context_init = false;
     g_sa_scan_context = -1;
     /* no longer "in" article scan mode */
-    g_sa_mode_read_elig = false;	/* the default */
+    g_sa_mode_read_elig = false;        /* the default */
     g_sa_in = false;
 }
 
 void sa_growarts(long oldlast, long last)
 {
     for (int i = oldlast + 1; i <= last; i++)
-	(void)sa_add_ent(i);
+        (void)sa_add_ent(i);
 }
 
 /* Initialize the scan-context to enter article scan mode. */
 void sa_init_context()
 {
     if (s_sa_context_init)
-	return;		/* already initialized */
+        return;         /* already initialized */
 
     if (g_sa_scan_context == -1)
-	g_sa_scan_context = s_new_context(S_ART);
+        g_sa_scan_context = s_new_context(S_ART);
     s_change_context(g_sa_scan_context);
     s_sa_context_init = true;
 }
@@ -126,8 +126,8 @@ bool sa_initarts()
     sa_init_ents();
     /* add all available articles to entry list */
     for (int a = article_first(g_absfirst); a <= g_lastart; a = article_next(a)) {
-	if (article_exists(a))
-	    (void)sa_add_ent(a);
+        if (article_exists(a))
+            (void)sa_add_ent(a);
     }
     return true;
 }
@@ -138,7 +138,7 @@ void sa_initmode()
     /* set up screen sizes */
     sa_set_screen();
 
-    g_sa_mode_zoom = false;			/* reset zoom */
+    g_sa_mode_zoom = false;                     /* reset zoom */
 }
 
 sa_main_result sa_mainloop()
@@ -152,27 +152,27 @@ sa_main_result sa_mainloop()
      * If that fails then strn will just use arrival ordering.
      */
     if (!g_sc_initialized && g_sa_mode_order == SA_ORDER_DESCENDING) {
-	g_sc_delay = false;	/* yes, actually score... */
-	sc_init(true);		/* wait for articles to score */
-	if (!g_sc_initialized)
+        g_sc_delay = false;     /* yes, actually score... */
+        sc_init(true);          /* wait for articles to score */
+        if (!g_sc_initialized)
             g_sa_mode_order = SA_ORDER_ARRIVAL; /* arrival order */
     }
     /* redraw it *all* */
     g_s_ref_all = true;
     if (g_s_top_ent < 1)
-	g_s_top_ent = s_first();
+        g_s_top_ent = s_first();
     int i = s_fillpage();
     if (i == -1 || i == 0) {
-	/* for now just quit if no page could be filled. */
-	return SA_QUIT;
+        /* for now just quit if no page could be filled. */
+        return SA_QUIT;
     }
     i = s_cmdloop();
     if (i == SA_READ) {
-	i = SA_NORM;
+        i = SA_NORM;
     }
     if (i > 0) {
-	g_sa_art = g_sa_ents[i].artnum;
-	return SA_NORM;
+        g_sa_art = g_sa_ents[i].artnum;
+        return SA_NORM;
     }
     /* something else (quit, return, etc...) */
     return static_cast<sa_main_result>(i);
@@ -182,9 +182,9 @@ sa_main_result sa_mainloop()
 void sa_lookahead()
 {
 #ifdef PENDING
-    sc_lookahead(true,false);		/* do resorting now... */
+    sc_lookahead(true,false);           /* do resorting now... */
 #else /* !PENDING */
-    ;				/* so the function isn't empty */
+    ;                           /* so the function isn't empty */
 #endif
 }
 
@@ -193,12 +193,12 @@ long sa_readmarked_elig()
 {
     long e = s_first();
     if (!e)
-	return 0L;
+        return 0L;
     for ( ; e; e = s_next(e)) {
-	if (!sa_basic_elig(e))
-	    continue;
-	if (sa_marked(e))
-	    return e;
+        if (!sa_basic_elig(e))
+            continue;
+        if (sa_marked(e))
+            return e;
     }
     /* This is possible since the marked articles might not be eligible. */
     return 0;
