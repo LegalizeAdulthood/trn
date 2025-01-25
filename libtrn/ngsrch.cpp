0d8b264498996b47b7cfd53c137964129b28e04a
Richard <legalize@xmission.com>
2025-01-24 19:31:50 -0700

    Expand tabs in ngsrch.cpp

--- libtrn/ngsrch.cpp
+++ libtrn/ngsrch.cpp
@@ -33,8 +33,8 @@ ng_search_result ng_search(char *patbuf, bool get_cmd)
 {
     g_int_count = 0;
     if (get_cmd && g_buf == patbuf)
-	if (!finish_command(false))	/* get rest of command */
-	    return NGS_ABORT;
+        if (!finish_command(false))     /* get rest of command */
+            return NGS_ABORT;
 
     perform_status_init(g_newsgroup_toread);
     char const cmdchr = *patbuf;         /* what kind of search? */
@@ -44,128 +44,128 @@ ng_search_result ng_search(char *patbuf, bool get_cmd)
     {
     }
     if (*pattern)
-	s_ng_doempty = false;
+        s_ng_doempty = false;
 
-    if (*s) {				/* modifiers or commands? */
-	while (*++s) {
-	    switch (*s) {
-	    case 'r':
-		s_ng_doempty = true;
-		break;
-	    default:
-		goto loop_break;
-	    }
-	}
+    if (*s) {                           /* modifiers or commands? */
+        while (*++s) {
+            switch (*s) {
+            case 'r':
+                s_ng_doempty = true;
+                break;
+            default:
+                goto loop_break;
+            }
+        }
       loop_break:;
     }
     while (isspace(*s) || *s == ':')
-	s++;
+        s++;
     char *cmdlst = nullptr; /* list of commands to do */
     if (*s)
-	cmdlst = savestr(s);
+        cmdlst = savestr(s);
     else if (g_general_mode == GM_SELECTOR)
-	cmdlst = savestr("+");
+        cmdlst = savestr("+");
     ng_search_result ret = NGS_NOTFOUND; /* assume no commands */
     if (cmdlst)
-	ret = NGS_DONE;
+        ret = NGS_DONE;
     const char *err = ng_comp(&s_ngcompex, pattern, true, true);
     if (err != nullptr)
     {
-					/* compile regular expression */
-	errormsg(err);
+                                        /* compile regular expression */
+        errormsg(err);
         if (cmdlst)
-	    free(cmdlst);
+            free(cmdlst);
         return NGS_ERROR;
     }
     if (!cmdlst) {
-	fputs("\nSearching...",stdout);	/* give them something to read */
-	fflush(stdout);
+        fputs("\nSearching...",stdout); /* give them something to read */
+        fflush(stdout);
     }
 
     bool const output_level = (!g_use_threads && g_general_mode != GM_SELECTOR);
     if (g_first_addgroup) {
-	ADDGROUP *gp = g_first_addgroup;
-	do {
-	    if (execute(&s_ngcompex,gp->name) != nullptr) {
-		if (!cmdlst)
-		    return NGS_FOUND;
-		if (addgrp_perform(gp,cmdlst,output_level && g_page_line==1)<0) {
-		    free(cmdlst);
-		    return NGS_INTR;
-		}
-	    }
-	    if (!output_level && g_page_line == 1)
-		perform_status(g_newsgroup_toread, 50);
-	} while ((gp = gp->next) != nullptr);
+        ADDGROUP *gp = g_first_addgroup;
+        do {
+            if (execute(&s_ngcompex,gp->name) != nullptr) {
+                if (!cmdlst)
+                    return NGS_FOUND;
+                if (addgrp_perform(gp,cmdlst,output_level && g_page_line==1)<0) {
+                    free(cmdlst);
+                    return NGS_INTR;
+                }
+            }
+            if (!output_level && g_page_line == 1)
+                perform_status(g_newsgroup_toread, 50);
+        } while ((gp = gp->next) != nullptr);
         if (cmdlst)
-	    free(cmdlst);
+            free(cmdlst);
         return ret;
     }
 
     bool const backward = cmdchr == '?'; /* direction of search */
     NGDATA const *ng_start = g_ngptr;
     if (backward) {
-	if (!g_ngptr)
-	{
+        if (!g_ngptr)
+        {
             g_ngptr = g_last_ng;
             ng_start = g_last_ng;
-	}
-	else if (!cmdlst) {
-	    if (g_ngptr == g_first_ng)	/* skip current newsgroup */
-		g_ngptr = g_last_ng;
-	    else
-		g_ngptr = g_ngptr->prev;
-	}
+        }
+        else if (!cmdlst) {
+            if (g_ngptr == g_first_ng)  /* skip current newsgroup */
+                g_ngptr = g_last_ng;
+            else
+                g_ngptr = g_ngptr->prev;
+        }
     }
     else {
-	if (!g_ngptr)
-	{
+        if (!g_ngptr)
+        {
             g_ngptr = g_first_ng;
             ng_start = g_first_ng;
-	}
-	else if (!cmdlst) {
-	    if (g_ngptr == g_last_ng)	/* skip current newsgroup */
-		g_ngptr = g_first_ng;
-	    else
-		g_ngptr = g_ngptr->next;
-	}
+        }
+        else if (!cmdlst) {
+            if (g_ngptr == g_last_ng)   /* skip current newsgroup */
+                g_ngptr = g_first_ng;
+            else
+                g_ngptr = g_ngptr->next;
+        }
     }
 
     if (!g_ngptr)
-	return NGS_NOTFOUND;
+        return NGS_NOTFOUND;
 
     do {
-	if (g_int_count) {
-	    g_int_count = 0;
-	    ret = NGS_INTR;
-	    break;
-	}
+        if (g_int_count) {
+            g_int_count = 0;
+            ret = NGS_INTR;
+            break;
+        }
 
-	if (g_ngptr->toread >= TR_NONE && ng_wanted(g_ngptr)) {
-	    if (g_ngptr->toread == TR_NONE)
-		set_toread(g_ngptr, ST_LAX);
-	    if (s_ng_doempty || ((g_ngptr->toread > TR_NONE) ^ g_sel_rereading)) {
-		if (!cmdlst)
-		    return NGS_FOUND;
-		set_ng(g_ngptr);
-		if (ng_perform(cmdlst,output_level && g_page_line == 1) < 0) {
-		    free(cmdlst);
-		    return NGS_INTR;
-		}
-	    }
-	    if (output_level && !cmdlst) {
-		printf("\n[0 unread in %s -- skipping]",g_ngptr->rcline);
-		fflush(stdout);
-	    }
-	}
-	if (!output_level && g_page_line == 1)
-	    perform_status(g_newsgroup_toread, 50);
+        if (g_ngptr->toread >= TR_NONE && ng_wanted(g_ngptr)) {
+            if (g_ngptr->toread == TR_NONE)
+                set_toread(g_ngptr, ST_LAX);
+            if (s_ng_doempty || ((g_ngptr->toread > TR_NONE) ^ g_sel_rereading)) {
+                if (!cmdlst)
+                    return NGS_FOUND;
+                set_ng(g_ngptr);
+                if (ng_perform(cmdlst,output_level && g_page_line == 1) < 0) {
+                    free(cmdlst);
+                    return NGS_INTR;
+                }
+            }
+            if (output_level && !cmdlst) {
+                printf("\n[0 unread in %s -- skipping]",g_ngptr->rcline);
+                fflush(stdout);
+            }
+        }
+        if (!output_level && g_page_line == 1)
+            perform_status(g_newsgroup_toread, 50);
     } while ((g_ngptr = (backward? (g_ngptr->prev? g_ngptr->prev : g_last_ng)
-			       : (g_ngptr->next? g_ngptr->next : g_first_ng)))
-		!= ng_start);
+                               : (g_ngptr->next? g_ngptr->next : g_first_ng)))
+                != ng_start);
 
     if (cmdlst)
-	free(cmdlst);
+        free(cmdlst);
     return ret;
 }
 
@@ -181,24 +181,24 @@ const char *ng_comp(COMPEX *compex, const char *pattern, bool RE, bool fold)
     char* d = ng_pattern;
 
     if (!*s) {
-	if (compile(compex, "", RE, fold))
-	    return "No previous search pattern";
-	return nullptr;			/* reuse old pattern */
+        if (compile(compex, "", RE, fold))
+            return "No previous search pattern";
+        return nullptr;                 /* reuse old pattern */
     }
     for (; *s; s++) {
-	if (*s == '.') {
-	    *d++ = '\\';
-	    *d++ = *s;
-	}
-	else if (*s == '?') {
-	    *d++ = '.';
-	}
-	else if (*s == '*') {
-	    *d++ = '.';
-	    *d++ = *s;
-	}
-	else
-	    *d++ = *s;
+        if (*s == '.') {
+            *d++ = '\\';
+            *d++ = *s;
+        }
+        else if (*s == '?') {
+            *d++ = '.';
+        }
+        else if (*s == '*') {
+            *d++ = '.';
+            *d++ = *s;
+        }
+        else
+            *d++ = *s;
     }
     *d = '\0';
     return compile(compex,ng_pattern,RE,fold);
