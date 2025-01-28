d67c6c25b01af0430441bc184df29810703d28f3
Richard <legalize@xmission.com>
2025-01-28 13:03:40 -0700

    Use winflexbison dependency instead of SourceForge ZIP

--- win-get-bison.bat
+++
@@ -1,3 +0,0 @@
-cmake -P win-get-bison.cmake
-@if errorlevel 1 exit /b 1
-set PATH=%PATH%;%CD%\..\bison-2.4.1\bin
--- win-get-bison.cmake
+++
@@ -1,4 +0,0 @@
-set(ZIP_FILE "../bison-2.4.1.zip")
-file(DOWNLOAD "http://user.xmission.com/~legalize/trn/bison-2.4.1.zip" "${ZIP_FILE}"
-    EXPECTED_MD5 "6d728df45f1cf60f5569da4963322ec3")
-file(ARCHIVE_EXTRACT INPUT "${ZIP_FILE}" DESTINATION "../bison-2.4.1")
