720b08f85765cf559bb3383984d38bfe487c6aeb
Richard <legalize@xmission.com>
2025-04-15 22:58:12 -0600

    Allman brace style in rt-wumpus.cpp

--- libtrn/rt-wumpus.cpp
+++ libtrn/rt-wumpus.cpp
@@ -93,7 +93,8 @@ void init_tree()
     /* Enumerate our subjects for display */
     SUBJECT *sp = thread->subj;
     int      num = 0;
-    do {
+    do
+    {
         sp->misc = num++;
         sp = sp->thread_link;
     } while (sp != thread->subj);
@@ -109,7 +110,8 @@ void init_tree()
     {
         s_first_depth = 0;
     }
-    else {
+    else
+    {
         if (s_my_depth+2 > s_max_depth)
         {
             s_first_depth = s_max_depth - 5;
@@ -125,7 +127,8 @@ void init_tree()
     {
         s_first_line = 0;
     }
-    else {
+    else
+    {
         if (s_my_line + g_max_tree_lines/2 > s_max_line)
         {
             s_first_line = s_max_line - (g_max_tree_lines - 1);
@@ -159,8 +162,10 @@ void init_tree()
 static void find_depth(ARTICLE *article, int depth)
 {
     s_max_depth = std::max(depth, s_max_depth);
-    for (;;) {
-        if (article == s_tree_article) {
+    for (;;)
+    {
+        if (article == s_tree_article)
+        {
             s_my_depth = depth;
             s_my_line = s_max_line;
         }
@@ -186,20 +191,26 @@ static void cache_tree(ARTICLE *ap, int depth, char *cp)
     int depth_mode;
 
     cp[1] = ' ';
-    if (depth >= s_first_depth && depth <= s_max_depth) {
+    if (depth >= s_first_depth && depth <= s_max_depth)
+    {
         cp += 5;
         depth_mode = 1;
-    } else if (depth+1 == s_first_depth)
+    }
+    else if (depth + 1 == s_first_depth)
     {
         depth_mode = 2;
     }
-    else {
+    else
+    {
         cp = s_tree_indent;
         depth_mode = 0;
     }
-    for (;;) {
-        switch (depth_mode) {
-        case 1: {
+    for (;;)
+    {
+        switch (depth_mode)
+        {
+        case 1:
+        {
             char ch;
 
             *s_str++ = ((ap->flags & AF_HAS_RE) || ap->parent) ? '-' : ' ';
@@ -207,13 +218,18 @@ static void cache_tree(ARTICLE *ap, int depth, char *cp)
             {
                 *s_str++ = '*';
             }
-            if (!(ap->flags & AF_UNREAD)) {
+            if (!(ap->flags & AF_UNREAD))
+            {
                 *s_str++ = '(';
                 ch = ')';
-            } else if (!g_selected_only || (ap->flags & AF_SEL)) {
+            }
+            else if (!g_selected_only || (ap->flags & AF_SEL))
+            {
                 *s_str++ = '[';
                 ch = ']';
-            } else {
+            }
+            else
+            {
                 *s_str++ = '<';
                 ch = '>';
             }
@@ -245,15 +261,19 @@ static void cache_tree(ARTICLE *ap, int depth, char *cp)
         default:
             break;
         }
-        if (ap->child1) {
+        if (ap->child1)
+        {
             cache_tree(ap->child1, depth+1, cp);
             cp[1] = '\0';
-        } else {
+        }
+        else
+        {
             if (!s_node_on_line && s_first_line == s_line_num)
             {
                 s_first_line++;
             }
-            if (s_line_num >= s_first_line) {
+            if (s_line_num >= s_first_line)
+            {
                 if (s_str[-1] == ' ')
                 {
                     s_str--;
@@ -262,7 +282,8 @@ static void cache_tree(ARTICLE *ap, int depth, char *cp)
                 s_tree_lines[s_line_num-s_first_line]
                         = safemalloc(s_str-s_tree_buff + 1);
                 std::strcpy(s_tree_lines[s_line_num - s_first_line], s_tree_buff);
-                if (s_node_on_line) {
+                if (s_node_on_line)
+                {
                     s_node_line_cnt = s_line_num - s_first_line;
                 }
             }
@@ -310,12 +331,14 @@ ARTICLE *get_tree_artp(int x, int y)
 */
 static ARTICLE *find_artp(ARTICLE *article, int x)
 {
-    for (;;) {
+    for (;;)
+    {
         if (!x && !s_find_artp_y)
         {
             return article;
         }
-        if (article->child1) {
+        if (article->child1)
+        {
             ARTICLE* ap = find_artp(article->child1, x-1);
             if (ap)
             {
@@ -365,7 +388,8 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
     }
 
     /* Copy line, filtering encoded and control characters. */
-    if (header_conv()) {
+    if (header_conv())
+    {
         tmpbuf = safemalloc(len * 2 + 2);
         line = tmpbuf + len + 1;
     }
@@ -378,25 +402,30 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
     {
         end = line + decode_header(line, orig_line, len);
     }
-    else {
+    else
+    {
         safecpy(line, orig_line, len+1);
         dectrl(line);
         end = line + len;
     }
-    if (header_conv()) {
+    if (header_conv())
+    {
         end = tmpbuf + strcharsubst(tmpbuf, line, len*2+2, *g_charsubst);
         line = tmpbuf;
     }
 
-    if (!*line) {
+    if (!*line)
+    {
         std::strcpy(line, " ");
         end = line+1;
     }
 
     color_object(COLOR_HEADER, true);
     /* If this is the first subject line, output it with a preceeding [1] */
-    if (is_subject && !std::isspace(*line)) {
-        if (g_threaded_group) {
+    if (is_subject && !std::isspace(*line))
+    {
+        if (g_threaded_group)
+        {
             color_object(COLOR_TREE_MARK, true);
             std::putchar('[');
             std::putchar(thread_letter(g_curr_artp));
@@ -405,13 +434,17 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
             std::putchar(' ');
             s_header_indent = 4;
         }
-        else {
+        else
+        {
             std::fputs("Subject: ", stdout);
             s_header_indent = 9;
         }
         i = 0;
-    } else {
-        if (*line != ' ') {
+    }
+    else
+    {
+        if (*line != ' ')
+        {
             /* A "normal" header line -- output keyword and set s_header_indent
             ** _except_ for the first line, which is a non-standard header.
             */
@@ -419,7 +452,8 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
             {
                 s_header_indent = 0;
             }
-            else {
+            else
+            {
                 *cp = '\0';
                 std::fputs(line, stdout);
                 std::putchar(' ');
@@ -431,15 +465,19 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
                 }
             }
             i = 0;
-        } else {
+        }
+        else
+        {
             /* Skip whitespace of continuation lines and prepare to indent */
             line = skip_eq(++line, ' ');
             i = s_header_indent;
         }
     }
-    for ( ; *line; i = s_header_indent) {
+    for (; *line; i = s_header_indent)
+    {
         maybe_eol();
-        if (i) {
+        if (i)
+        {
             std::putchar('+');
             while (--i)
             {
@@ -458,17 +496,21 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
         }
         /* Figure padding between header and tree output, wrapping long lines */
         int pad_cnt = wrap_at - (end - line + s_header_indent);
-        if (pad_cnt <= 0) {
+        if (pad_cnt <= 0)
+        {
             cp = line + (int)(wrap_at - s_header_indent - 1);
             pad_cnt = 1;
-            while (cp > line && *cp != ' ') {
-                if (*--cp == ',' || *cp == '.' || *cp == '-' || *cp == '!') {
+            while (cp > line && *cp != ' ')
+            {
+                if (*--cp == ',' || *cp == '.' || *cp == '-' || *cp == '!')
+                {
                     cp++;
                     break;
                 }
                 pad_cnt++;
             }
-            if (cp == line) {
+            if (cp == line)
+            {
                 cp += wrap_at - s_header_indent;
                 pad_cnt = 0;
             }
@@ -477,7 +519,9 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
             /* keep rn's backpager happy */
             vwtary(g_artline, vrdary(g_artline - 1));
             g_artline++;
-        } else {
+        }
+        else
+        {
             cp = end;
             ch = '\0';
         }
@@ -501,8 +545,10 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
         }
         line = cp;
         /* Check if we've got any tree lines to output */
-        if (wrap_at != g_tc_COLS-1 && header_line <= s_max_line) {
-            do {
+        if (wrap_at != g_tc_COLS - 1 && header_line <= s_max_line)
+        {
+            do
+            {
                 std::putchar(' ');
             } while (pad_cnt--);
             g_term_col = wrap_at;
@@ -525,15 +571,19 @@ int tree_puts(char *orig_line, ART_LINE header_line, int is_subject)
             /* Handle standout output for '*' and '@' marked nodes, then
             ** continue with the rest of the line.
             */
-            while (cp1 || cp2) {
+            while (cp1 || cp2)
+            {
                 color_object(COLOR_TREE_MARK, true);
-                if (cp1 && (!cp2 || cp1 < cp2)) {
+                if (cp1 && (!cp2 || cp1 < cp2))
+                {
                     cp = cp1;
                     cp1 = nullptr;
                     *cp++ = '*';
                     std::putchar(*cp++);
                     std::putchar(*cp++);
-                } else {
+                }
+                else
+                {
                     cp = cp2;
                     cp2 = nullptr;
                     *cp++ = '@';
@@ -566,7 +616,8 @@ int  finish_tree(ART_LINE last_line)
 {
     ART_LINE start_line = last_line;
 
-    while (last_line <= s_max_line) {
+    while (last_line <= s_max_line)
+    {
         g_artline++;
         last_line += tree_puts("+", last_line, 0);
         vwtary(g_artline, g_artpos);    /* keep rn's backpager happy */
@@ -578,7 +629,8 @@ int  finish_tree(ART_LINE last_line)
 */
 void entire_tree(ARTICLE* ap)
 {
-    if (!ap) {
+    if (!ap)
+    {
         if (g_verbose)
         {
             std::fputs("\nNo article tree to display.\n", stdout);
@@ -591,12 +643,14 @@ void entire_tree(ARTICLE* ap)
         return;
     }
 
-    if (!g_threaded_group) {
+    if (!g_threaded_group)
+    {
         g_threaded_group = true;
         std::printf("Threading the group. ");
         std::fflush(stdout);
         thread_open();
-        if (!g_threaded_group) {
+        if (!g_threaded_group)
+        {
             std::printf("*failed*\n");
             termdown(1);
             return;
@@ -617,7 +671,8 @@ void entire_tree(ARTICLE* ap)
     /* Enumerate our subjects for display */
     SUBJECT *sp = thread->subj;
     int      num = 0;
-    do {
+    do
+    {
         if (check_page_line())
         {
             return;
@@ -658,45 +713,59 @@ static void display_tree(ARTICLE *article, char *cp)
     cp[1] = ' ';
     cp += 5;
     color_object(COLOR_TREE, true);
-    for (;;) {
+    for (;;)
+    {
         std::putchar(((article->flags&AF_HAS_RE) || article->parent) ? '-' : ' ');
-        if (!(article->flags & AF_UNREAD)) {
+        if (!(article->flags & AF_UNREAD))
+        {
             g_buf[0] = '(';
             g_buf[2] = ')';
-        } else if (!g_selected_only || (article->flags & AF_SEL)) {
+        }
+        else if (!g_selected_only || (article->flags & AF_SEL))
+        {
             g_buf[0] = '[';
             g_buf[2] = ']';
-        } else {
+        }
+        else
+        {
             g_buf[0] = '<';
             g_buf[2] = '>';
         }
         g_buf[1] = thread_letter(article);
-        if (article == g_curr_artp) {
+        if (article == g_curr_artp)
+        {
             color_string(COLOR_TREE_MARK,g_buf);
-        } else if (article == g_recent_artp) {
+        }
+        else if (article == g_recent_artp)
+        {
             std::putchar(g_buf[0]);
             color_object(COLOR_TREE_MARK, true);
             std::putchar(g_buf[1]);
             color_pop();        /* of COLOR_TREE_MARK */
             std::putchar(g_buf[2]);
-        } else
+        }
+        else
         {
             std::fputs(g_buf, stdout);
         }
 
-        if (article->sibling) {
+        if (article->sibling)
+        {
             *cp = '|';
-        } else
+        }
+        else
         {
             *cp = ' ';
         }
-        if (article->child1) {
+        if (article->child1)
+        {
             std::putchar((article->child1->sibling)? '+' : '-');
             color_pop();        /* of COLOR_TREE */
             display_tree(article->child1, cp);
             color_object(COLOR_TREE, true);
             cp[1] = '\0';
-        } else
+        }
+        else
         {
             newline();
         }
@@ -709,7 +778,8 @@ static void display_tree(ARTICLE *article, char *cp)
             *cp = '\\';
         }
         s_tree_indent[5] = ' ';
-        if (check_page_line()) {
+        if (check_page_line())
+        {
             color_pop();
             return;
         }
