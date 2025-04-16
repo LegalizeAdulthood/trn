5b9463d81c76ffccc7cd85c19226fe842e7173cf
Richard <legalize@xmission.com>
2025-04-16 09:31:22 -0600

    Align case labels in smisc.cpp

--- libtrn/smisc.cpp
+++ libtrn/smisc.cpp
@@ -20,9 +20,10 @@ bool s_eligible(long ent)
 {
     switch (g_s_cur_type)
     {
-      case S_ART:
+    case S_ART:
         return sa_eligible(ent);
-      default:
+
+    default:
         std::printf("s_eligible: current type is bad!\n");
         return false;
     }
@@ -42,9 +43,10 @@ const char *s_get_statchars(long ent, int line)
     }
     switch (g_s_cur_type)
     {
-      case S_ART:
+    case S_ART:
         return sa_get_statchars(ent,line);
-      default:
+
+    default:
         return nullptr;
     }
 }
@@ -53,9 +55,10 @@ const char *s_get_desc(long ent, int line, bool trunc)
 {
     switch (g_s_cur_type)
     {
-      case S_ART:
+    case S_ART:
         return sa_get_desc(ent,line,trunc);
-      default:
+
+    default:
         return nullptr;
     }
 }
@@ -64,9 +67,10 @@ int s_ent_lines(long ent)
 {
     switch (g_s_cur_type)
     {
-      case S_ART:
+    case S_ART:
         return sa_ent_lines(ent);
-      default:
+
+    default:
         return 1;
     }
 }
