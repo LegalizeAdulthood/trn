fa89259126dcf0f1f13d85ba3a0694155a6020c2
Richard <legalize@xmission.com>
2025-04-17 16:52:51 -0700

    Use lower_case names for static functions and data: mempool

--- libtrn/mempool.cpp
+++ libtrn/mempool.cpp
@@ -24,9 +24,9 @@ enum
 struct MemoryPoolFragment
 {
     char *data;
-    char *lastfree;
-    long bytesfree;
-    int next;
+    char *last_free;
+    long  bytes_free;
+    int   next;
 };
 
 /* structure for extensibility */
@@ -35,9 +35,9 @@ struct MemoryPoolHead
     int current; /* index into mp_frag of most recent alloc */
 };
 
-static MemoryPoolFragment s_mpfrags[MAX_MEM_FRAGS]{}; /* zero is unused */
-static int     s_mp_first_free_frag{};
-static MemoryPoolHead s_mpheads[MAX_MEM_POOLS]{};
+static MemoryPoolFragment s_mp_frags[MAX_MEM_FRAGS]{}; /* zero is unused */
+static int                s_mp_first_free_frag{};
+static MemoryPoolHead     s_mp_heads[MAX_MEM_POOLS]{};
 
 static int mp_alloc_frag();
 static void mp_free_frag(int f);
@@ -47,15 +47,15 @@ void mp_init()
     int i;
     for (i = 1; i < MAX_MEM_FRAGS; i++)
     {
-        s_mpfrags[i].data = nullptr;
-        s_mpfrags[i].lastfree = nullptr;
-        s_mpfrags[i].bytesfree = 0;
-        s_mpfrags[i].next = i+1;
+        s_mp_frags[i].data = nullptr;
+        s_mp_frags[i].last_free = nullptr;
+        s_mp_frags[i].bytes_free = 0;
+        s_mp_frags[i].next = i+1;
     }
-    s_mpfrags[i-1].next = 0;
+    s_mp_frags[i-1].next = 0;
     s_mp_first_free_frag = 1;   /* first free fragment */
 
-    for (MemoryPoolHead &pool : s_mpheads)
+    for (MemoryPoolHead &pool : s_mp_heads)
     {
         pool.current = 0;
     }
@@ -70,14 +70,14 @@ static int mp_alloc_frag()
         std::printf("trn: out of memory pool fragments!\n");
         sig_catcher(0);         /* die. */
     }
-    s_mp_first_free_frag = s_mpfrags[f].next;
-    if (s_mpfrags[f].bytesfree)
+    s_mp_first_free_frag = s_mp_frags[f].next;
+    if (s_mp_frags[f].bytes_free)
     {
         return f;       /* already allocated */
     }
-    s_mpfrags[f].data = (char*)safe_malloc(FRAG_SIZE);
-    s_mpfrags[f].lastfree = s_mpfrags[f].data;
-    s_mpfrags[f].bytesfree = FRAG_SIZE;
+    s_mp_frags[f].data = (char*)safe_malloc(FRAG_SIZE);
+    s_mp_frags[f].last_free = s_mp_frags[f].data;
+    s_mp_frags[f].bytes_free = FRAG_SIZE;
 
     return f;
 }
@@ -95,10 +95,10 @@ static void mp_free_frag(int f)
     s_mpfrags[f].bytesfree = 0;
 #else
     /* instead of freeing it, reset it for later use */
-    s_mpfrags[f].lastfree = s_mpfrags[f].data;
-    s_mpfrags[f].bytesfree = FRAG_SIZE;
+    s_mp_frags[f].last_free = s_mp_frags[f].data;
+    s_mp_frags[f].bytes_free = FRAG_SIZE;
 #endif
-    s_mpfrags[f].next = s_mp_first_free_frag;
+    s_mp_frags[f].next = s_mp_first_free_frag;
     s_mp_first_free_frag = f;
 }
 
@@ -120,19 +120,19 @@ char *mp_save_str(const char *str, MemoryPool pool)
         std::printf("trn: (maximum length allowed is %d)\n",FRAG_SIZE);
         sig_catcher(0);         /* die. */
     }
-    int f = s_mpheads[pool].current;
+    int f = s_mp_heads[pool].current;
     /* just to be extra safe, keep 2 bytes unused at end of block */
-    if (f == 0 || len >= s_mpfrags[f].bytesfree - 2)
+    if (f == 0 || len >= s_mp_frags[f].bytes_free - 2)
     {
         int oldf = f;
         f = mp_alloc_frag();
-        s_mpfrags[f].next = oldf;
-        s_mpheads[pool].current = f;
+        s_mp_frags[f].next = oldf;
+        s_mp_heads[pool].current = f;
     }
-    char *s = s_mpfrags[f].lastfree;
+    char *s = s_mp_frags[f].last_free;
     safe_copy(s,str,len+1);
-    s_mpfrags[f].lastfree += len+1;
-    s_mpfrags[f].bytesfree -= len+1;
+    s_mp_frags[f].last_free += len+1;
+    s_mp_frags[f].bytes_free -= len+1;
     return s;
 }
 
@@ -149,29 +149,29 @@ char *mp_malloc(int len, MemoryPool pool)
         std::printf("trn: (maximum length allowed is %d)\n",FRAG_SIZE);
         sig_catcher(0);         /* die. */
     }
-    int f = s_mpheads[pool].current;
-    if (f == 0 || len >= s_mpfrags[f].bytesfree)
+    int f = s_mp_heads[pool].current;
+    if (f == 0 || len >= s_mp_frags[f].bytes_free)
     {
         int oldf = f;
         f = mp_alloc_frag();
-        s_mpfrags[f].next = oldf;
-        s_mpheads[pool].current = f;
+        s_mp_frags[f].next = oldf;
+        s_mp_heads[pool].current = f;
     }
-    char *s = s_mpfrags[f].lastfree;
-    s_mpfrags[f].lastfree += len+1;
-    s_mpfrags[f].bytesfree -= len+1;
+    char *s = s_mp_frags[f].last_free;
+    s_mp_frags[f].last_free += len+1;
+    s_mp_frags[f].bytes_free -= len+1;
     return s;
 }
 
 /* free a whole memory pool */
 void mp_free(MemoryPool pool)
 {
-    int f = s_mpheads[pool].current;
+    int f = s_mp_heads[pool].current;
     while (f)
     {
-        int oldnext = s_mpfrags[f].next;
+        int oldnext = s_mp_frags[f].next;
         mp_free_frag(f);
         f = oldnext;
     }
-    s_mpheads[pool].current = 0;
+    s_mp_heads[pool].current = 0;
 }
