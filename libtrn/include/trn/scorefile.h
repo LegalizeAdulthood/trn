11d9aa78c38ca0c5e4775b9ff468ca4f01741886
Richard <legalize@xmission.com>
2025-04-17 11:28:05 -0600

    Use lower_case names for global functions and data: scorefile.h

--- libtrn/include/trn/scorefile.h
+++ libtrn/include/trn/scorefile.h
@@ -14,25 +14,25 @@ struct CompiledRegex;
 struct ScoreFileEntry
 {
     HeaderLineType head_type; /* header # (see trn/head.h) */
-    int score;                  /* score change */
-    char *str1;                 /* first string part */
-    char *str2;                 /* second string part */
-    CompiledRegex *compex;             /* regular expression ptr */
-    char flags;                 /* 1: regex is valid
-                                 * 2: rule has been applied to the current article.
-                                 * 4: use faster rule checking  (later)
-                                 */
+    int            score;     /* score change */
+    char          *str1;      /* first string part */
+    char          *str2;      /* second string part */
+    CompiledRegex *compex;    /* regular expression ptr */
+    char           flags;     /* 1: regex is valid
+                               * 2: rule has been applied to the current article.
+                               * 4: use faster rule checking  (later)
+                               */
 };
 /* note that negative header #s are used to indicate special entries... */
 
 /* for cached score rules */
 struct ScoreFile
 {
-    char* fname;
-    int num_lines;
-    int num_alloc;
-    long line_on;
-    char** lines;
+    char  *fname;
+    int    num_lines;
+    int    num_alloc;
+    long   line_on;
+    char **lines;
 };
 
 extern int  g_sf_num_entries;   /* # of entries */
@@ -57,17 +57,17 @@ inline bool is_text_zero(const char *s)
 
 char *sf_get_filename(int level);
 char *sf_cmd_fname(char *s);
-bool sf_do_command(char *cmd, bool check);
+bool  sf_do_command(char *cmd, bool check);
 char *sf_freeform(char *start1, char *end1);
-bool sf_do_line(char *line, bool check);
-void sf_do_file(const char *fname);
-int score_match(char *str, int ind);
-int sf_score(ArticleNum a);
+bool  sf_do_line(char *line, bool check);
+void  sf_do_file(const char *fname);
+int   score_match(char *str, int ind);
+int   sf_score(ArticleNum a);
 char *sf_missing_score(const char *line);
-void sf_append(char *line);
+void  sf_append(char *line);
 char *sf_get_line(ArticleNum a, HeaderLineType h);
-void sf_print_match(int indx);
-void sf_exclude_file(const char *fname);
-void sf_edit_file(const char *filespec);
+void  sf_print_match(int indx);
+void  sf_exclude_file(const char *fname);
+void  sf_edit_file(const char *filespec);
 
 #endif
