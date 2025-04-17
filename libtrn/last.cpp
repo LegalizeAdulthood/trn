43bce89f7ac8570de19b99397bde8d286661d236
Richard <legalize@xmission.com>
2025-04-17 16:51:13 -0700

    Use lower_case names for static functions and data: last

--- libtrn/last.cpp
+++ libtrn/last.cpp
@@ -16,32 +16,32 @@
 #include <ctime>
 #include <string>
 
-std::string g_last_newsgroup_name;    /* last newsgroup read */
-long        g_last_time{};    /* time last we ran */
+std::string g_last_newsgroup_name; /* last newsgroup read */
+long        g_last_time{};         /* time last we ran */
 long        g_last_active_size{};  /* last known size of active file */
-long        g_last_new_time{}; /* time of last newgroup request */
+long        g_last_new_time{};     /* time of last newgroup request */
 long        g_last_extra_num{};
 
-static char *s_lastfile{}; /* path name of .rnlast file */
-static long  s_starttime{};
+static char *s_last_file{}; /* path name of .rnlast file */
+static long  s_start_time{};
 
 void last_init()
 {
-    s_lastfile = save_str(file_exp(LASTNAME));
+    s_last_file = save_str(file_exp(LASTNAME));
 
-    s_starttime = (long)std::time(nullptr);
+    s_start_time = (long)std::time(nullptr);
     read_last();
 }
 
 void last_final()
 {
-    safe_free0(s_lastfile);
+    safe_free0(s_last_file);
     g_last_newsgroup_name.clear();
 }
 
 void read_last()
 {
-    if (std::FILE *fp = std::fopen(s_lastfile, "r"))
+    if (std::FILE *fp = std::fopen(s_last_file, "r"))
     {
         if (std::fgets(g_buf, sizeof g_buf, fp) != nullptr)
         {
@@ -55,7 +55,7 @@ void read_last()
                                            &g_last_new_time,&g_last_extra_num);
             if (!g_last_new_time)
             {
-                g_last_new_time = s_starttime;
+                g_last_new_time = s_start_time;
             }
             g_last_time = std::max(old_last, g_last_time);
         }
@@ -67,16 +67,16 @@ void read_last()
 
 void write_last()
 {
-    std::sprintf(g_buf,"%s.%ld", s_lastfile, g_our_pid);
+    std::sprintf(g_buf,"%s.%ld", s_last_file, g_our_pid);
     if (std::FILE *fp = std::fopen(g_buf, "w"))
     {
-        g_last_time = std::max(g_last_time, s_starttime);
+        g_last_time = std::max(g_last_time, s_start_time);
         std::fprintf(fp,"%s\n%ld\n%ld\n%ld\n%ld\n",
                 g_newsgroup_name.c_str(),g_last_time,
                 g_last_active_size,g_last_new_time,g_last_extra_num);
         std::fclose(fp);
-        remove(s_lastfile);
-        rename(g_buf,s_lastfile);
+        remove(s_last_file);
+        rename(g_buf,s_last_file);
     }
     else
     {
