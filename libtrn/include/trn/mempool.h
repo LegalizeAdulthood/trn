5fa2a0da1d88788285f88d94a553d54dcd60a60c
Richard <legalize@xmission.com>
2025-04-18 14:41:36 -0700

    Use // style comments: mempool.h

--- libtrn/include/trn/mempool.h
+++ libtrn/include/trn/mempool.h
@@ -4,13 +4,13 @@
 #ifndef TRN_MEMPOOL_H
 #define TRN_MEMPOOL_H
 
-/* memory pool numbers */
+// memory pool numbers
 
 enum MemoryPool
 {
-    MP_SCORE1 = 0,  /* scoring rule text */
-    MP_SCORE2 = 1,  /* scorefile cache */
-    MP_SATHREAD = 2 /* sathread.c storage */
+    MP_SCORE1 = 0,  // scoring rule text
+    MP_SCORE2 = 1,  // scorefile cache
+    MP_SATHREAD = 2 // sathread.c storage
 };
 
 void mp_init();
