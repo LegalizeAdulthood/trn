4c8e088c45b5d1f804805e6435085b12f42f4c1d
Richard <legalize@xmission.com>
2025-04-18 14:41:42 -0700

    Use // style comments: sdisp.cpp

--- libtrn/sdisp.cpp
+++ libtrn/sdisp.cpp
@@ -1,4 +1,4 @@
-/* This file Copyright 1992 by Clifford A. Adams */
+// This file Copyright 1992 by Clifford A. Adams
 /* sdisp.c
  *
  * display stuff
@@ -7,7 +7,7 @@
 #include "config/common.h"
 #include "trn/sdisp.h"
 
-#include "trn/ng.h" /* g_mailcall */
+#include "trn/ng.h" // g_mailcall
 #include "trn/sadisp.h"
 #include "trn/scan.h"
 #include "trn/smisc.h"
@@ -16,9 +16,9 @@
 
 #include <cstdio>
 
-int  g_scr_height{}; /* height of screen in characters */
-int  g_scr_width{};  /* width of screen in characters */
-bool g_s_resized{};  /* has the window been resized? */
+int  g_scr_height{}; // height of screen in characters
+int  g_scr_width{};  // width of screen in characters
+bool g_s_resized{};  // has the window been resized?
 
 void s_goxy(int x, int y)
 {
@@ -35,15 +35,15 @@ void s_mail_and_place()
     bool next;
 
 #ifdef MAIL_CALL
-    set_mail(false);             /* another chance to check mail */
+    set_mail(false);             // another chance to check mail
     std::printf("%s",g_mail_call);
-#endif /* MAILCALL */
-    /* print page status wrt all entries */
+#endif // MAILCALL
+    // print page status wrt all entries
     previous = (0 != s_prev_elig(g_page_ents[0].ent_num));
     next = (0 != s_next_elig(g_page_ents[g_s_bot_ent].ent_num));
     if (previous && next)
     {
-        std::printf("-MIDDLE-");             /* middle of entries */
+        std::printf("-MIDDLE-");             // middle of entries
     }
     else if (next && !previous)
     {
@@ -53,7 +53,7 @@ void s_mail_and_place()
     {
         std::printf("-BOTTOM-");
     }
-    else        /* !previous && !next */
+    else        // !previous && !next
     {
         std::printf("-ALL-");
     }
@@ -73,7 +73,7 @@ void s_refresh_top()
 
 void s_refresh_bot()
 {
-    /* if bottom bar exists, then it is at least one character high... */
+    // if bottom bar exists, then it is at least one character high...
     s_goxy(0,g_tc_LINES-g_s_bot_lines);
     switch (g_s_cur_type)
     {
@@ -84,14 +84,14 @@ void s_refresh_bot()
     g_s_ref_bot = false;
 }
 
-/* refresh both status and description */
+// refresh both status and description
 void s_refresh_ent_zone()
 {
-    int start;          /* starting page_arts index to refresh... */
+    int start;          // starting page_arts index to refresh...
 
     if (g_s_ref_status < g_s_ref_desc)
     {
-        /* refresh status characters up to (not including) desc_line */
+        // refresh status characters up to (not including) desc_line
         for (int i = g_s_ref_status; i <= g_s_bot_ent && i < g_s_ref_desc; i++)
         {
             s_refresh_description(i);
@@ -110,9 +110,9 @@ void s_refresh_ent_zone()
     {
         s_ref_entry(i,i==start);
     }
-    /* clear to end of screen */
+    // clear to end of screen
     clear_rest();
-    /* now we need to redraw the bottom status line */
+    // now we need to redraw the bottom status line
     g_s_ref_bot = true;
     g_s_ref_status = -1;
     g_s_ref_desc = -1;
@@ -126,12 +126,12 @@ void s_place_ptr()
     std::fflush(stdout);
 }
 
-/* refresh the status line for an article on screen page */
-/* note: descriptions will not (for now) be individually refreshable */
+// refresh the status line for an article on screen page
+// note: descriptions will not (for now) be individually refreshable
 void s_refresh_status(int line)
 {
     long ent = g_page_ents[line].ent_num;
-    TRN_ASSERT(line <= g_s_bot_ent);    /* better be refreshing on-page */
+    TRN_ASSERT(line <= g_s_bot_ent);    // better be refreshing on-page
     s_goxy(0,g_s_top_lines+g_page_ents[line].start_line);
     int j = g_page_ents[line].lines;
     for (int i = 1; i <= j; i++)
@@ -144,16 +144,16 @@ void s_refresh_status(int line)
 void s_refresh_description(int line)
 {
     long ent = g_page_ents[line].ent_num;
-    TRN_ASSERT(line <= g_s_bot_ent);    /* better be refreshing on-page */
+    TRN_ASSERT(line <= g_s_bot_ent);    // better be refreshing on-page
     int startline = g_s_top_lines + g_page_ents[line].start_line;
     int j = g_page_ents[line].lines;
     for (int i = 1; i <= j; i++)
     {
         s_goxy(g_s_status_cols+g_s_cursor_cols,(i-1)+startline);
-        /* allow flexible format later? */
+        // allow flexible format later?
         if (g_s_item_num_cols)
         {
-            if (i == 1)         /* first description line */
+            if (i == 1)         // first description line
             {
                 if (line < 99)
                 {
@@ -161,7 +161,7 @@ void s_refresh_description(int line)
                 }
                 else
                 {
-                    std::printf("** ");      /* too big */
+                    std::printf("** ");      // too big
                 }
             }
             else
@@ -176,11 +176,11 @@ void s_refresh_description(int line)
     std::fflush(stdout);
 }
 
-//int jump;     /* true means that the cursor should be positioned */
+//int jump;     // true means that the cursor should be positioned
 void s_ref_entry(int line, int jump)
 {
     long ent = g_page_ents[line].ent_num;
-    TRN_ASSERT(line <= g_s_bot_ent);    /* better be refreshing on-page */
+    TRN_ASSERT(line <= g_s_bot_ent);    // better be refreshing on-page
     if (jump)
     {
         s_goxy(0,g_s_top_lines+g_page_ents[line].start_line);
@@ -188,11 +188,11 @@ void s_ref_entry(int line, int jump)
     int j = g_page_ents[line].lines;
     for (int i = 1; i <= j; i++)
     {
-        /* later replace middle with variable #spaces routine */
+        // later replace middle with variable #spaces routine
         std::printf("%s%s",s_get_statchars(ent,i),"  ");
         if (g_s_item_num_cols)
         {
-            if (i == 1)         /* first description line */
+            if (i == 1)         // first description line
             {
                 if (line < 99)
                 {
@@ -200,7 +200,7 @@ void s_ref_entry(int line, int jump)
                 }
                 else
                 {
-                    std::printf("** ");      /* too big */
+                    std::printf("** ");      // too big
                 }
             }
             else
@@ -223,7 +223,7 @@ void s_refresh()
 {
     if (g_s_ref_all)
     {
-        clear();        /* make a clean slate */
+        clear();        // make a clean slate
         g_s_ref_desc = 0;
         g_s_ref_status = 0;
     }
@@ -263,21 +263,21 @@ void s_refresh()
 
 int s_init_screen()
 {
-    /* check to see if term is too dumb: if so, return non-zero */
+    // check to see if term is too dumb: if so, return non-zero
 
-    /* set scr_{height,width} */
-    /* return 0 if all went well */
+    // set scr_{height,width}
+    // return 0 if all went well
 
     g_scr_height = g_tc_LINES;
     g_scr_width = g_tc_COLS;
-    if (g_scr_height > 2 && g_scr_width > 1)    /* current dependencies */
+    if (g_scr_height > 2 && g_scr_width > 1)    // current dependencies
     {
-        return 0;       /* everything is OK. */
+        return 0;       // everything is OK.
     }
-    return 1;   /* we can't play with this... */
+    return 1;   // we can't play with this...
 }
 
-/* screen-refresh the status if on-page */
+// screen-refresh the status if on-page
 void s_ref_status_on_page(long ent)
 {
     for (int i = 0; i <= g_s_bot_ent; i++)
@@ -295,7 +295,7 @@ void s_resize_win()
     int i;
 
     i = s_initscreen();
-    /* later possibly use the return value for an error abort? */
+    // later possibly use the return value for an error abort?
     g_s_resized = true;
 #endif
 }
