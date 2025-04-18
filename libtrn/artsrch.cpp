e366f33008e5a04eefc064811ef102cd29f3490a
Richard <legalize@xmission.com>
2025-04-18 14:41:33 -0700

    Use // style comments: artsrch.cpp

--- libtrn/artsrch.cpp
+++ libtrn/artsrch.cpp
@@ -1,6 +1,6 @@
 /* artsrch.c
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 
 
 #include "config/common.h"
@@ -29,18 +29,18 @@
 #include <cstring>
 #include <string>
 
-static CompiledRegex s_sub_compex{}; /* last compiled subject search */
-static CompiledRegex s_art_compex{}; /* last compiled normal search */
+static CompiledRegex s_sub_compex{}; // last compiled subject search
+static CompiledRegex s_art_compex{}; // last compiled normal search
 
 static bool wanted(CompiledRegex *compex, ArticleNum art_num, ArtScope scope);
 
-std::string    g_last_pat;                  /* last search pattern */
-CompiledRegex *g_bra_compex{&s_art_compex}; /* current compex with brackets */
+std::string    g_last_pat;                  // last search pattern
+CompiledRegex *g_bra_compex{&s_art_compex}; // current compex with brackets
 const char    *g_scope_str{"sfHhbBa"};      //
-ArtScope       g_art_how_much{};            /* search scope */
-HeaderLineType g_art_srch_hdr{};            /* specific header number to search */
-bool           g_art_do_read{};             /* search read articles? */
-bool           g_kill_thru_kludge{true};    /* -k */
+ArtScope       g_art_how_much{};            // search scope
+HeaderLineType g_art_srch_hdr{};            // specific header number to search
+bool           g_art_do_read{};             // search read articles?
+bool           g_kill_thru_kludge{true};    // -k
 
 void art_search_init()
 {
@@ -48,34 +48,34 @@ void art_search_init()
     init_compex(&s_art_compex);
 }
 
-/* search for an article containing some pattern */
+// search for an article containing some pattern
 
-/* if patbuf != g_buf, get_cmd must be set to false!!! */
+// if patbuf != g_buf, get_cmd must be set to false!!!
 ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
 {
-    char* pattern;                      /* unparsed pattern */
-    char cmd_chr = *pat_buf;              /* what kind of search? */
+    char* pattern;                      // unparsed pattern
+    char cmd_chr = *pat_buf;              // what kind of search?
     bool backward = cmd_chr == '?' || cmd_chr == Ctl('p');
-                                        /* direction of search */
-    CompiledRegex* compex;                     /* which compiled expression */
-    char* cmd_lst = nullptr;             /* list of commands to do */
-    ArtSearchResult ret = SRCH_NOT_FOUND; /* assume no commands */
-    int salt_away = 0;                   /* store in KILL file? */
-    ArtScope how_much;                  /* search scope: subj/from/Hdr/head/art */
-    HeaderLineType search_header;           /* header to search if Hdr scope */
+                                        // direction of search
+    CompiledRegex* compex;                     // which compiled expression
+    char* cmd_lst = nullptr;             // list of commands to do
+    ArtSearchResult ret = SRCH_NOT_FOUND; // assume no commands
+    int salt_away = 0;                   // store in KILL file?
+    ArtScope how_much;                  // search scope: subj/from/Hdr/head/art
+    HeaderLineType search_header;           // header to search if Hdr scope
     bool top_start = false;
-    bool do_read;                        /* search read articles? */
-    bool fold_case = true;               /* fold upper and lower case? */
-    int ignore_thru = 0;                 /* should we ignore the thru line? */
+    bool do_read;                        // search read articles?
+    bool fold_case = true;               // fold upper and lower case?
+    int ignore_thru = 0;                 // should we ignore the thru line?
     bool output_level = (!g_use_threads && g_general_mode != GM_SELECTOR);
     ArticleNum search_first;
 
     g_int_count = 0;
-    if (cmd_chr == '/' || cmd_chr == '?')         /* normal search? */
+    if (cmd_chr == '/' || cmd_chr == '?')         // normal search?
     {
         if (get_cmd && g_buf == pat_buf)
         {
-            if (!finish_command(false)) /* get rest of command */
+            if (!finish_command(false)) // get rest of command
             {
                 return SRCH_ABORT;
             }
@@ -93,65 +93,65 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
             search_header = g_art_srch_hdr;
             do_read = g_art_do_read;
         }
-        char *s = copy_till(g_buf,pat_buf+1,cmd_chr);/* ok to cpy g_buf+1 to g_buf */
+        char *s = copy_till(g_buf,pat_buf+1,cmd_chr);// ok to cpy g_buf+1 to g_buf
         pattern = g_buf;
         if (*pattern)
         {
             g_last_pat = pattern;
         }
-        if (*s)                         /* modifiers or commands? */
+        if (*s)                         // modifiers or commands?
         {
             while (*++s)
             {
                 switch (*s)
                 {
-                case 'f':               /* scan the From line */
+                case 'f':               // scan the From line
                     how_much = ARTSCOPE_FROM;
                     break;
 
-                case 'H':               /* scan a specific header */
+                case 'H':               // scan a specific header
                     how_much = ARTSCOPE_ONE_HDR;
                     s = copy_till(g_msg, s+1, ':');
                     search_header = get_header_num(g_msg);
                     goto loop_break;
 
-                case 'h':               /* scan header */
+                case 'h':               // scan header
                     how_much = ARTSCOPE_HEAD;
                     break;
 
-                case 'b':               /* scan body sans signature */
+                case 'b':               // scan body sans signature
                     how_much = ARTSCOPE_BODY_NO_SIG;
                     break;
 
-                case 'B':               /* scan body */
+                case 'B':               // scan body
                     how_much = ARTSCOPE_BODY;
                     break;
 
-                case 'a':               /* scan article */
+                case 'a':               // scan article
                     how_much = ARTSCOPE_ARTICLE;
                     break;
 
-                case 't':               /* start from the top */
+                case 't':               // start from the top
                     top_start = true;
                     break;
 
-                case 'r':               /* scan read articles */
+                case 'r':               // scan read articles
                     do_read = true;
                     break;
 
-                case 'K':               /* put into KILL file */
+                case 'K':               // put into KILL file
                     salt_away = 1;
                     break;
 
-                case 'c':               /* make search case sensitive */
+                case 'c':               // make search case sensitive
                     fold_case = false;
                     break;
 
-                case 'I':               /* ignore the killfile thru line */
+                case 'I':               // ignore the killfile thru line
                     ignore_thru = 1;
                     break;
 
-                case 'N':               /* override ignore if -k was used */
+                case 'N':               // override ignore if -k was used
                     ignore_thru = -1;
                     break;
 
@@ -171,7 +171,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
             {
                 do_read = true;
             }
-            if (*s == 'k')              /* grandfather clause */
+            if (*s == 'k')              // grandfather clause
             {
                 *s = 'j';
             }
@@ -205,12 +205,12 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
         {
             std::strcpy(pattern,": *");
             h = pattern + std::strlen(pattern);
-            interp(h,pat_buf_siz - (h-pat_buf),"%\\s");  /* fetch current subject */
+            interp(h,pat_buf_siz - (h-pat_buf),"%\\s");  // fetch current subject
         }
         else
         {
             h = pattern;
-            /* TODO: if using thread files, make this "%\\)f" */
+            // TODO: if using thread files, make this "%\\)f"
             interp(pattern, pat_buf_siz - 1, "%\\>f");
         }
         if (cmd_chr == 'k' || cmd_chr == 'K' || cmd_chr == ',' //
@@ -251,7 +251,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
                 {
                     cmd_lst = save_str("j");
                 }
-                mark_as_read(article_ptr(g_art));       /* this article needs to die */
+                mark_as_read(article_ptr(g_art));       // this article needs to die
             }
             if (!*h)
             {
@@ -285,7 +285,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
             g_search_ahead = -1;
         }
 
-        {                       /* compensate for notesfiles */
+        {                       // compensate for notesfiles
             for (int i = 24; *h && i--; h++)
             {
                 if (*h == '\\')
@@ -307,7 +307,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
         const char *s = compile(compex, pattern, true, fold_case);
         if (s != nullptr)
         {
-            /* compile regular expression */
+            // compile regular expression
             error_msg(s);
             ret = SRCH_ABORT;
             goto exit;
@@ -315,13 +315,13 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
     }
     if (cmd_lst && std::strchr(cmd_lst,'='))
     {
-        ret = SRCH_ERROR;               /* listing subjects is an error? */
+        ret = SRCH_ERROR;               // listing subjects is an error?
     }
     if (g_general_mode == GM_SELECTOR)
     {
         if (!cmd_lst)
         {
-            if (g_sel_mode == SM_ARTICLE)/* set the selector's default command */
+            if (g_sel_mode == SM_ARTICLE)// set the selector's default command
             {
                 cmd_lst = save_str("+");
             }
@@ -391,7 +391,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
             std::fputs("\nSearching...\n",stdout);
             term_down(2);
         }
-                                        /* give them something to read */
+                                        // give them something to read
     }
     if (ignore_thru == 0 && g_kill_thru_kludge && cmd_lst //
         && (*cmd_lst == '+' || *cmd_lst == '.'))
@@ -409,7 +409,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
     {
         if (cmd_lst && g_art <= g_last_art)
         {
-            g_art++;                    /* include current article */
+            g_art++;                    // include current article
         }
     }
     else
@@ -420,7 +420,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
         }
         else if (cmd_lst && g_art >= g_abs_first)
         {
-            g_art--;                    /* include current article */
+            g_art--;                    // include current article
         }
     }
     if (g_search_ahead > 0)
@@ -435,7 +435,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
     perform_status_init(g_newsgroup_ptr->to_read);
     while (true)
     {
-        /* check if we're out of articles */
+        // check if we're out of articles
         if (backward? ((g_art = article_prev(g_art)) < search_first)
                     : ((g_art = article_next(g_art)) > g_last_art))
         {
@@ -452,7 +452,7 @@ ArtSearchResult art_search(char *pat_buf, int pat_buf_siz, bool get_cmd)
         {
             if (wanted(compex, g_art, how_much))
             {
-                                    /* does the shoe fit? */
+                                    // does the shoe fit?
                 if (!cmd_lst)
                 {
                     return SRCH_FOUND;
@@ -482,8 +482,8 @@ exit:
     return ret;
 }
 
-/* determine if article fits pattern */
-/* returns true if it exists and fits pattern, false otherwise */
+// determine if article fits pattern
+// returns true if it exists and fits pattern, false otherwise
 
 static bool wanted(CompiledRegex *compex, ArticleNum art_num, ArtScope scope)
 {
@@ -531,10 +531,10 @@ static bool wanted(CompiledRegex *compex, ArticleNum art_num, ArtScope scope)
             {
                 return false;
             }
-            /* see if it's in the header */
-            if (execute(compex,g_head_buf))      /* does it match? */
+            // see if it's in the header
+            if (execute(compex,g_head_buf))      // does it match?
             {
-                return true;                    /* say, "Eureka!" */
+                return true;                    // say, "Eureka!"
             }
             if (scope < ARTSCOPE_ARTICLE)
             {
@@ -559,7 +559,7 @@ static bool wanted(CompiledRegex *compex, ArticleNum art_num, ArtScope scope)
                 return false;
             }
         }
-        /* loop through each line of the article */
+        // loop through each line of the article
         seek_art_buf(g_header_type[PAST_HEADER].min_pos);
         while ((s = read_art_buf(false)) != nullptr)
         {
@@ -583,12 +583,12 @@ static bool wanted(CompiledRegex *compex, ArticleNum art_num, ArtScope scope)
             {
                 *nl_ptr = ch;
             }
-            if (success && !in_sig)             /* does it match? */
+            if (success && !in_sig)             // does it match?
             {
-                return true;                    /* say, "Eureka!" */
+                return true;                    // say, "Eureka!"
             }
         }
-        return false;                           /* out of article, so no match */
+        return false;                           // out of article, so no match
     }
     }
     return execute(compex,g_buf) != nullptr;
