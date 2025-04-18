72fcf4edfb00b42624d76a1e5150a2fc6ae9d47f
Richard <legalize@xmission.com>
2025-04-18 14:41:36 -0700

    Use // style comments: only.h

--- libtrn/include/trn/only.h
+++ libtrn/include/trn/only.h
@@ -1,6 +1,6 @@
 /* trn/only.h
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 #ifndef TRN_ONLY_H
 #define TRN_ONLY_H
 
@@ -10,11 +10,11 @@
 #include "trn/search.h"
 #endif
 
-constexpr int MAX_NG_TO_DO{512}; /* number of newsgroups allowed on command line */
+constexpr int MAX_NG_TO_DO{512}; // number of newsgroups allowed on command line
 
-extern char *g_newsgroup_to_do[MAX_NG_TO_DO]; /* restrictions in effect */
-extern int   g_max_newsgroup_to_do;           /*  0 => no restrictions */
-                                              /* >0 => # of entries in g_ngtodo */
+extern char *g_newsgroup_to_do[MAX_NG_TO_DO]; // restrictions in effect
+extern int   g_max_newsgroup_to_do;           // 0 => no restrictions
+                                              // >0 => # of entries in g_ngtodo
 extern char g_empty_only_char;
 
 void only_init();
