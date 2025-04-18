205d80d4e5e450a0672de8517be2f7637250666d
Richard <legalize@xmission.com>
2025-04-18 14:41:41 -0700

    Use // style comments: samain.cpp

--- libtrn/samain.cpp
+++ libtrn/samain.cpp
@@ -1,4 +1,4 @@
-/* This file Copyright 1992 by Clifford A. Adams */
+// This file Copyright 1992 by Clifford A. Adams
 /* samain.c
  *
  * main working routines  (may change later)
@@ -24,33 +24,33 @@
 
 #include <cstdlib>
 
-bool g_sa_mode_zoom{};      /* true if in "zoom" (display only selected) mode */
-int  g_sa_scan_context{-1}; /* contains the scan-context number for the current article scan */
+bool g_sa_mode_zoom{};      // true if in "zoom" (display only selected) mode
+int  g_sa_scan_context{-1}; // contains the scan-context number for the current article scan
 
 static int  s_sa_ents_alloc{};
-static bool s_sa_context_init{}; /* has context been initialized? */
+static bool s_sa_context_init{}; // has context been initialized?
 
 void sa_init()
 {
     sa_init_context();
     if (g_last_art == 0 || g_abs_first > g_last_art)
     {
-        return;         /* no articles */
+        return;         // no articles
     }
-    if (s_init_screen())         /* If not able to init screen...*/
+    if (s_init_screen())         // If not able to init screen...
     {
-        return;                         /* ...most likely dumb terminal */
+        return;                         // ...most likely dumb terminal
     }
-    sa_init_mode();                      /*      mode differences */
+    sa_init_mode();                      // mode differences
     sa_init_threads();
     g_sa_mode_read_elig = false;
-    if (g_first_art > g_last_art)         /* none unread */
+    if (g_first_art > g_last_art)         // none unread
     {
-        g_sa_mode_read_elig = true;     /* unread+read in some situations */
+        g_sa_mode_read_elig = true;     // unread+read in some situations
     }
-    if (!sa_init_arts())                 /* init article array(s) */
+    if (!sa_init_arts())                 // init article array(s)
     {
-        return;                         /* ... no articles */
+        return;                         // ... no articles
     }
 #ifdef PENDING
     if (g_sa_mode_read_elig)
@@ -60,7 +60,7 @@ void sa_init()
     }
 #endif
     s_save_context();
-    g_sa_initialized = true;            /* all went well... */
+    g_sa_initialized = true;            // all went well...
 }
 
 void sa_init_ents()
@@ -75,17 +75,17 @@ void sa_clean_ents()
     std::free(g_sa_ents);
 }
 
-/* returns entry number that was added */
-//ART_NUM artnum;               /* article number to be added */
+// returns entry number that was added
+//ART_NUM artnum;               // article number to be added
 long sa_add_ent(ArticleNum artnum)
 {
     g_sa_num_ents++;
     if (g_sa_num_ents > s_sa_ents_alloc)
     {
         s_sa_ents_alloc += 100;
-        if (s_sa_ents_alloc == 100)     /* newly allocated */
+        if (s_sa_ents_alloc == 100)     // newly allocated
         {
-            /* don't use number 0, just allocate it and skip it */
+            // don't use number 0, just allocate it and skip it
             g_sa_num_ents = 2;
             g_sa_ents = (ScanArticleEntryData*)safe_malloc(s_sa_ents_alloc
                                         * sizeof (ScanArticleEntryData));
@@ -108,13 +108,13 @@ void sa_clean_main()
 {
     sa_clean_ents();
 
-    g_sa_mode_zoom = false;     /* doesn't survive across groups */
-    /* remove the now-unused scan-context */
+    g_sa_mode_zoom = false;     // doesn't survive across groups
+    // remove the now-unused scan-context
     s_delete_context(g_sa_scan_context);
     s_sa_context_init = false;
     g_sa_scan_context = -1;
-    /* no longer "in" article scan mode */
-    g_sa_mode_read_elig = false;        /* the default */
+    // no longer "in" article scan mode
+    g_sa_mode_read_elig = false;        // the default
     g_sa_in = false;
 }
 
@@ -126,12 +126,12 @@ void sa_grow_arts(long oldlast, long last)
     }
 }
 
-/* Initialize the scan-context to enter article scan mode. */
+// Initialize the scan-context to enter article scan mode.
 void sa_init_context()
 {
     if (s_sa_context_init)
     {
-        return;         /* already initialized */
+        return;         // already initialized
     }
 
     if (g_sa_scan_context == -1)
@@ -145,7 +145,7 @@ void sa_init_context()
 bool sa_init_arts()
 {
     sa_init_ents();
-    /* add all available articles to entry list */
+    // add all available articles to entry list
     for (int a = article_first(g_abs_first); a <= g_last_art; a = article_next(a))
     {
         if (article_exists(a))
@@ -156,13 +156,13 @@ bool sa_init_arts()
     return true;
 }
 
-/* note: initscreen must be called before (for g_scr_width) */
+// note: initscreen must be called before (for g_scr_width)
 void sa_init_mode()
 {
-    /* set up screen sizes */
+    // set up screen sizes
     sa_set_screen();
 
-    g_sa_mode_zoom = false;                     /* reset zoom */
+    g_sa_mode_zoom = false;                     // reset zoom
 }
 
 SaMainResult sa_main_loop()
@@ -177,14 +177,14 @@ SaMainResult sa_main_loop()
      */
     if (!g_sc_initialized && g_sa_mode_order == SA_ORDER_DESCENDING)
     {
-        g_sc_delay = false;     /* yes, actually score... */
-        sc_init(true);          /* wait for articles to score */
+        g_sc_delay = false;     // yes, actually score...
+        sc_init(true);          // wait for articles to score
         if (!g_sc_initialized)
         {
-            g_sa_mode_order = SA_ORDER_ARRIVAL; /* arrival order */
+            g_sa_mode_order = SA_ORDER_ARRIVAL; // arrival order
         }
     }
-    /* redraw it *all* */
+    // redraw it *all*
     g_s_ref_all = true;
     if (g_s_top_ent < 1)
     {
@@ -193,7 +193,7 @@ SaMainResult sa_main_loop()
     int i = s_fill_page();
     if (i == -1 || i == 0)
     {
-        /* for now just quit if no page could be filled. */
+        // for now just quit if no page could be filled.
         return SA_QUIT;
     }
     i = s_cmd_loop();
@@ -206,21 +206,21 @@ SaMainResult sa_main_loop()
         g_sa_art = g_sa_ents[i].artnum;
         return SA_NORM;
     }
-    /* something else (quit, return, etc...) */
+    // something else (quit, return, etc...)
     return static_cast<SaMainResult>(i);
 }
 
-/* do something useful until a key is pressed. */
+// do something useful until a key is pressed.
 void sa_lookahead()
 {
 #ifdef PENDING
-    sc_look_ahead(true,false);           /* do resorting now... */
-#else /* !PENDING */
-    ;                           /* so the function isn't empty */
+    sc_look_ahead(true,false);           // do resorting now...
+#else // !PENDING
+    ;                           // so the function isn't empty
 #endif
 }
 
-/* Returns first marked entry number, or 0 if no articles are marked. */
+// Returns first marked entry number, or 0 if no articles are marked.
 long sa_read_marked_elig()
 {
     long e = s_first();
@@ -239,6 +239,6 @@ long sa_read_marked_elig()
             return e;
         }
     }
-    /* This is possible since the marked articles might not be eligible. */
+    // This is possible since the marked articles might not be eligible.
     return 0;
 }
