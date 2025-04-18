dc5c2d3d429d6f443f8a825a3993c37acf4876ee
Richard <legalize@xmission.com>
2025-04-18 14:41:34 -0700

    Use // style comments: autosub.h

--- libtrn/include/trn/autosub.h
+++ libtrn/include/trn/autosub.h
@@ -1,6 +1,6 @@
 /* trn/autosub.h
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 #ifndef TRN_AUTOSUB_H
 #define TRN_AUTOSUB_H
 
