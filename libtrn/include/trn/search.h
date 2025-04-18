a7e8d6fc7db06dd0680e32b4528143efae8f9a47
Richard <legalize@xmission.com>
2025-04-18 14:41:38 -0700

    Use // style comments: search.h

--- libtrn/include/trn/search.h
+++ libtrn/include/trn/search.h
@@ -1,25 +1,25 @@
 /* trn/search.h
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 #ifndef TRN_SEARCH_H
 #define TRN_SEARCH_H
 
 #ifndef NBRA
 #define NBRA    10              /* the maximum number of meta-brackets in an
                                    RE -- \( \) */
-#define NALTS   10              /* the maximum number of \|'s */
+#define NALTS   10              // the maximum number of \|'s
 
 struct CompiledRegex
 {
-    char* expbuf;               /* The compiled search string */
-    int eblen;                  /* Length of above buffer */
-    char* alternatives[NALTS+1];/* The list of \| seperated alternatives */
-    const char* braslist[NBRA]; /* RE meta-bracket start list */
-    const char* braelist[NBRA]; /* RE meta-bracket end list */
-    char* brastr;               /* saved match string after execute() */
+    char* expbuf;               // The compiled search string
+    int eblen;                  // Length of above buffer
+    char* alternatives[NALTS+1];// The list of \| seperated alternatives
+    const char* braslist[NBRA]; // RE meta-bracket start list
+    const char* braelist[NBRA]; // RE meta-bracket end list
+    char* brastr;               // saved match string after execute()
     char nbra;                  /* The number of meta-brackets int the most
                                    recenlty compiled RE */
-    bool do_folding;            /* fold upper and lower case? */
+    bool do_folding;            // fold upper and lower case?
 };
 #endif
 
