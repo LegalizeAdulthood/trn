0ba4d2107432dfab6df502c5bb988a3cd2cf2f41
Richard <legalize@xmission.com>
2025-04-18 14:41:42 -0700

    Use // style comments: spage.cpp

--- libtrn/spage.cpp
+++ libtrn/spage.cpp
@@ -1,4 +1,4 @@
-/* This file Copyright 1992 by Clifford A. Adams */
+// This file Copyright 1992 by Clifford A. Adams
 /* spage.c
  *
  */
@@ -9,47 +9,47 @@
 #include "trn/list.h"
 #include "trn/ngdata.h"
 #include "trn/cache.h"
-#include "trn/rt-util.h" /* spinner */
+#include "trn/rt-util.h" // spinner
 #include "trn/samain.h"
 #include "trn/scan.h"
 #include "trn/scanart.h"
-#include "trn/sdisp.h" /* for display dimension sizes */
+#include "trn/sdisp.h" // for display dimension sizes
 #include "trn/smisc.h"
 #include "trn/sorder.h"
 
 #include <algorithm>
 #include <cstdio>
 
-/* returns true if successful */
-//long end;             /* entry number to be last on page */
+// returns true if successful
+//long end;             // entry number to be last on page
 bool s_fill_page_backward(long end)
 {
-    int min_page_ents;  /* current minimum */
+    int min_page_ents;  // current minimum
     int  i;
     int  j;
-    long a;             /* misc entry number */
-    int page_lines;     /* lines available on page */
-    int line_on;        /* line # currently on: 0=line after top status bar */
+    long a;             // misc entry number
+    int page_lines;     // lines available on page
+    int line_on;        // line # currently on: 0=line after top status bar
 
-/* Debug */
+// Debug
 #if 0
     std::printf("entry: s_fillpage_backward(%d)\n",end);
 #endif
 
     page_lines = g_scr_height - g_s_top_lines - g_s_bot_lines;
     min_page_ents = MAX_PAGE_SIZE-1;
-    g_s_bot_ent = -1;   /* none yet */
+    g_s_bot_ent = -1;   // none yet
     line_on = 0;
 
-    /* whatever happens, the entry display will need a full refresh... */
+    // whatever happens, the entry display will need a full refresh...
     g_s_ref_status = 0;
-    g_s_ref_desc = 0;   /* refresh from top entry */
+    g_s_ref_desc = 0;   // refresh from top entry
 
-    if (end < 1)                /* start from end */
+    if (end < 1)                // start from end
     {
         end = s_last();
     }
-    if (end == 0)               /* no entries */
+    if (end == 0)               // no entries
     {
         return false;
     }
@@ -61,19 +61,19 @@ bool s_fill_page_backward(long end)
     {
         a = s_prev_elig(end);
     }
-    if (!a)     /* no eligible entries */
+    if (!a)     // no eligible entries
     {
         return false;
     }
-    /* at this point we *know* that there is at least one eligible */
+    // at this point we *know* that there is at least one eligible
 
-/* what if the "first" one for the page has a description too long? */
-/* later make it shorten the descript. */
+// what if the "first" one for the page has a description too long?
+// later make it shorten the descript.
 
-/* CONSIDER: make setspin conditional on context? */
-    set_spin(SPIN_BACKGROUND);   /* turn on spin on cache misses */
-    /* uncertain what next comment means now */
-    /* later do sheer paranoia check for min_page_ents */
+// CONSIDER: make setspin conditional on context?
+    set_spin(SPIN_BACKGROUND);   // turn on spin on cache misses
+    // uncertain what next comment means now
+    // later do sheer paranoia check for min_page_ents
     while ((line_on + s_ent_lines(a)) <= page_lines)
     {
         g_page_ents[min_page_ents].ent_num = a;
@@ -83,15 +83,15 @@ bool s_fill_page_backward(long end)
         g_s_bot_ent += 1;
         line_on = line_on+i;
         a = s_prev_elig(a);
-        if (!a)         /* no more eligible */
+        if (!a)         // no more eligible
         {
-            break;      /* get out of loop and finish up... */
+            break;      // get out of loop and finish up...
         }
     }
-/* what if none on page? (desc. too long) Fix later */
-    set_spin(SPIN_POP);  /* turn off spin on cache misses */
-    /* replace the entries at the front of the g_page_ents array */
-    /* also set start_line entries */
+// what if none on page? (desc. too long) Fix later
+    set_spin(SPIN_POP);  // turn off spin on cache misses
+    // replace the entries at the front of the g_page_ents array
+    // also set start_line entries
     j = 0;
     line_on = 0;
     for (int k = min_page_ents + 1; k < MAX_PAGE_SIZE; k++)
@@ -103,7 +103,7 @@ bool s_fill_page_backward(long end)
         line_on = line_on + g_page_ents[j].lines;
         j++;
     }
-    /* set new g_s_top_ent */
+    // set new g_s_top_ent
     g_s_top_ent = g_page_ents[0].ent_num;
     /* Now, suppose that the pointer position is off the page.  That would
      * be bad, so lets make sure it doesn't happen.
@@ -128,21 +128,21 @@ bool s_fill_page_backward(long end)
     {
         return s_fill_page_backward(end);
     }
-/* next time the unavail won't be chosen */
-    return true;        /* we have a page... */
+// next time the unavail won't be chosen
+    return true;        // we have a page...
 }
 
-/* fills the page array */
-/* returns true on success */
-//long start;                   /* entry to start filling with */
+// fills the page array
+// returns true on success
+//long start;                   // entry to start filling with
 bool s_fill_page_forward(long start)
 {
     int i;
     long a;
-    int page_lines;     /* lines available on page */
-    int line_on;        /* line # currently on (0: line after top status bar */
+    int page_lines;     // lines available on page
+    int line_on;        // line # currently on (0: line after top status bar
 
-/* Debug */
+// Debug
 #if 0
     std::printf("entry: s_fillpage_forward(%d)\n",start);
 #endif
@@ -151,15 +151,15 @@ bool s_fill_page_forward(long start)
     g_s_bot_ent = -1;
     line_on = 0;
 
-    /* whatever happens, the entry zone will need a full refresh... */
+    // whatever happens, the entry zone will need a full refresh...
     g_s_ref_status = 0;
     g_s_ref_desc = 0;
 
-    if (start < 0)      /* fill from top */
+    if (start < 0)      // fill from top
     {
         start = s_first();
     }
-    if (start == 0)     /* no entries */
+    if (start == 0)     // no entries
     {
         return false;
     }
@@ -172,17 +172,17 @@ bool s_fill_page_forward(long start)
     {
         a = s_next_elig(start);
     }
-    if (!a)     /* no eligible entries */
+    if (!a)     // no eligible entries
     {
         return false;
     }
-    /* at this point we *know* that there is at least one eligible */
+    // at this point we *know* that there is at least one eligible
 
-/* what if the first entry for the page has a description too long? */
-/* later make it shorten the descript. */
+// what if the first entry for the page has a description too long?
+// later make it shorten the descript.
 
-    set_spin(SPIN_BACKGROUND);   /* turn on spin on cache misses */
-/* ?  later do paranoia check for g_s_bot_ent */
+    set_spin(SPIN_BACKGROUND);   // turn on spin on cache misses
+// ?  later do paranoia check for g_s_bot_ent
     while ((line_on + s_ent_lines(a)) <= page_lines)
     {
         g_s_bot_ent += 1;
@@ -193,16 +193,16 @@ bool s_fill_page_forward(long start)
         g_page_ents[g_s_bot_ent].page_flags = (char)0;
         line_on = line_on+i;
         a = s_next_elig(a);
-        if (!a)         /* no more eligible */
+        if (!a)         // no more eligible
         {
-            break;      /* get out of loop and finish up... */
+            break;      // get out of loop and finish up...
         }
     }
-    set_spin(SPIN_POP);  /* turn off spin on cache misses */
+    set_spin(SPIN_POP);  // turn off spin on cache misses
     /* Now, suppose that the pointer position is off the page.  That would
      * be bad, so lets make sure it doesn't happen.
      */
-    /* set new g_s_top_ent */
+    // set new g_s_top_ent
     g_s_top_ent = g_page_ents[0].ent_num;
     g_s_ptr_page_line = std::min<long>(g_s_ptr_page_line, g_s_bot_ent);
     if (g_s_cur_type != S_ART)
@@ -224,8 +224,8 @@ bool s_fill_page_forward(long start)
     {
         return s_fill_page_forward(start);
     }
-    /* next time the unavail won't be chosen */
-    return true;        /* we have a page... */
+    // next time the unavail won't be chosen
+    return true;        // we have a page...
 }
 
 /* Given possible changes to which entries should be on the page,
@@ -233,16 +233,16 @@ bool s_fill_page_forward(long start)
  * (the last entry on the page will be refreshed whether it needs it
  *  or not.)
  */
-/* returns true on success */
+// returns true on success
 bool s_refill_page()
 {
     int  i;
     int  j;
     long a;
-    int page_lines;     /* lines available on page */
-    int line_on;        /* line # currently on: 0=line after top status bar */
+    int page_lines;     // lines available on page
+    int line_on;        // line # currently on: 0=line after top status bar
 
-/* Debug */
+// Debug
 #if 0
     std::printf("entry: s_refillpage\n");
 #endif
@@ -273,7 +273,7 @@ bool s_refill_page()
      */
     a = s_next_elig(g_page_ents[0].ent_num);
 
-    /* similar to the tests in the last loop... */
+    // similar to the tests in the last loop...
     while (i <= g_s_bot_ent && s_eligible(g_page_ents[i].ent_num) //
            && g_page_ents[i].ent_num == a                         //
            && g_page_ents[i].lines == s_ent_lines(g_page_ents[i].ent_num))
@@ -281,7 +281,7 @@ bool s_refill_page()
         i++;
         a = s_next_elig(a);
     }
-    j = i-1;    /* j is the last "good" entry */
+    j = i-1;    // j is the last "good" entry
 
     g_s_bot_ent = j;
     line_on = g_page_ents[j].start_line + s_ent_lines(g_page_ents[j].ent_num);
@@ -328,9 +328,9 @@ bool s_refill_page()
     }
     if (i <= g_s_bot_ent)
     {
-        return s_refill_page();  /* next time the unavail won't be chosen */
+        return s_refill_page();  // next time the unavail won't be chosen
     }
-    return true;        /* we have a page... */
+    return true;        // we have a page...
 }
 
 /* fills a page from current position.
@@ -344,40 +344,40 @@ int s_fill_page()
 {
     int i;
 
-    g_s_refill = false; /* we don't need one now */
-    if (g_s_top_ent < 1)        /* set top to first entry */
+    g_s_refill = false; // we don't need one now
+    if (g_s_top_ent < 1)        // set top to first entry
     {
         g_s_top_ent = s_first();
     }
-    if (g_s_top_ent == 0)       /* no entries */
+    if (g_s_top_ent == 0)       // no entries
     {
-        return 0;       /* failure */
+        return 0;       // failure
     }
-    if (!s_refill_page())        /* try for efficient refill */
+    if (!s_refill_page())        // try for efficient refill
     {
         if (!s_fill_page_backward(g_s_top_ent))
         {
-            /* downgrade eligibility standards */
+            // downgrade eligibility standards
             switch (g_s_cur_type)
             {
-            case S_ART:               /* article context */
-                if (g_sa_mode_zoom)             /* we were zoomed in */
+            case S_ART:               // article context
+                if (g_sa_mode_zoom)             // we were zoomed in
                 {
-                    g_s_ref_top = true; /* for "FOLD" display */
-                    g_sa_mode_zoom = false;     /* zoom out */
+                    g_s_ref_top = true; // for "FOLD" display
+                    g_sa_mode_zoom = false;     // zoom out
                     if (g_sa_unzoom_refold)
                     {
                         g_sa_mode_fold = true;
                     }
-                    (void)s_go_top_ents();      /* go to top (ents and page) */
+                    (void)s_go_top_ents();      // go to top (ents and page)
                     return s_fill_page();
                 }
-                return -1;              /* there just aren't entries! */
+                return -1;              // there just aren't entries!
 
             default:
-                return -1;              /* there just aren't entries! */
-            } /* switch */
-        } /* if */
+                return -1;              // there just aren't entries!
+            } // switch
+        } // if
     }
     /* temporary fix.  Under some conditions ineligible entries will
      * not be found until they are in the page.  In this case just
@@ -387,14 +387,14 @@ int s_fill_page()
     {
         if (!s_eligible(g_page_ents[j].ent_num))
         {
-            return s_fill_page();  /* ineligible won't be chosen again */
+            return s_fill_page();  // ineligible won't be chosen again
         }
     }
     if (g_s_cur_type != S_ART)
     {
         return 1;
     }
-    /* be extra cautious about the article scan pages */
+    // be extra cautious about the article scan pages
     for (i = 0; i <= g_s_bot_ent; i++)
     {
         if (is_unavailable(g_sa_ents[g_page_ents[i].ent_num].artnum))
@@ -404,9 +404,9 @@ int s_fill_page()
     }
     if (i <= g_s_bot_ent)
     {
-        return s_fill_page();    /* next time the unavail won't be chosen */
+        return s_fill_page();    // next time the unavail won't be chosen
     }
-    return 1;   /* I guess everything worked :-) */
+    return 1;   // I guess everything worked :-)
 }
 
 void s_clean_page()
@@ -430,31 +430,31 @@ bool s_go_top_ents()
     {
         std::printf("s_go_top_ents(): no first entry\n");
     }
-    TRN_ASSERT(g_s_top_ent);    /* be nicer later */
-    if (!s_eligible(g_s_top_ent))       /* this may save a redraw...*/
+    TRN_ASSERT(g_s_top_ent);    // be nicer later
+    if (!s_eligible(g_s_top_ent))       // this may save a redraw...
     {
         g_s_top_ent = s_next_elig(g_s_top_ent);
     }
-    if (!g_s_top_ent)   /* none eligible */
+    if (!g_s_top_ent)   // none eligible
     {
-        /* just go to the top of all the entries */
+        // just go to the top of all the entries
         if (g_s_cur_type == S_ART)
         {
             g_s_top_ent = g_abs_first;
         }
         else
         {
-            g_s_top_ent = 1;    /* not very nice coding */
+            g_s_top_ent = 1;    // not very nice coding
         }
     }
     g_s_refill = true;
     s_go_top_page();
-    return true;        /* successful */
+    return true;        // successful
 }
 
 bool s_go_bot_ents()
 {
-    bool flag = s_fill_page_backward(s_last()); /* fill backwards */
+    bool flag = s_fill_page_backward(s_last()); // fill backwards
     if (!flag)
     {
         return false;
@@ -468,12 +468,12 @@ void s_go_next_page()
     long a = s_next_elig(g_page_ents[g_s_bot_ent].ent_num);
     if (!a)
     {
-        return;         /* no next page (we shouldn't have been called) */
+        return;         // no next page (we shouldn't have been called)
     }
-    /* the fill-page will set the refresh for the screen */
+    // the fill-page will set the refresh for the screen
     bool flag = s_fill_page_forward(a);
-    TRN_ASSERT(flag);           /* I *must* be able to fill a page */
-    g_s_ptr_page_line = 0;      /* top of page */
+    TRN_ASSERT(flag);           // I *must* be able to fill a page
+    g_s_ptr_page_line = 0;      // top of page
 }
 
 void s_go_prev_page()
@@ -481,15 +481,15 @@ void s_go_prev_page()
     long a = s_prev_elig(g_page_ents[0].ent_num);
     if (!a)
     {
-        return;         /* no prev. page (we shouldn't have been called) */
+        return;         // no prev. page (we shouldn't have been called)
     }
-    /* the fill-page will set the refresh for the screen */
-    bool flag = s_fill_page_backward(a); /* fill backward */
-    TRN_ASSERT(flag);                   /* be nicer later... */
-    /* take care of partially filled previous pages */
+    // the fill-page will set the refresh for the screen
+    bool flag = s_fill_page_backward(a); // fill backward
+    TRN_ASSERT(flag);                   // be nicer later...
+    // take care of partially filled previous pages
     flag = s_refill_page();
-    TRN_ASSERT(flag);           /* be nicer later... */
+    TRN_ASSERT(flag);           // be nicer later...
     g_s_ref_status = 0;
-    g_s_ref_desc = 0;      /* refresh from top */
-    g_s_ptr_page_line = 0; /* top of page */
+    g_s_ref_desc = 0;      // refresh from top
+    g_s_ptr_page_line = 0; // top of page
 }
