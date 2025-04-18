a9dd316a489f7a23a8300045fc8ec3c9a3471b56
Richard <legalize@xmission.com>
2025-04-18 14:41:35 -0700

    Use // style comments: last.h

--- libtrn/include/trn/last.h
+++ libtrn/include/trn/last.h
@@ -1,15 +1,15 @@
 /* trn/last.h
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 #ifndef TRN_LAST_H
 #define TRN_LAST_H
 
 #include <string>
 
-extern std::string g_last_newsgroup_name; /* last newsgroup read */
-extern long        g_last_time;           /* time last we ran */
-extern long        g_last_active_size;    /* last known size of active file */
-extern long        g_last_new_time;       /* time of last newgroup request */
+extern std::string g_last_newsgroup_name; // last newsgroup read
+extern long        g_last_time;           // time last we ran
+extern long        g_last_active_size;    // last known size of active file
+extern long        g_last_new_time;       // time of last newgroup request
 extern long        g_last_extra_num;
 
 void last_init();
