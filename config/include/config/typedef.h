ddb78e528859dc9a04f43f680dc364fd9572d8c2
Richard <legalize@xmission.com>
2025-04-18 14:41:32 -0700

    Use // style comments: typedef.h

--- config/include/config/typedef.h
+++ config/include/config/typedef.h
@@ -3,16 +3,16 @@
 #ifndef TRN_TYPEDEF_H
 #define TRN_TYPEDEF_H
 
-/* some important types */
+// some important types
 
-using NewsgroupNum = int;        /* newsgroup number */
-using ArticleNum = long;         /* article number */
-using ArticleUnread = long;      /* could be short to save space */
-using ArticlePosition = long;    /* char position in article file */
-using ArticleLine = int;         /* line position in article file */
-using ActivePosition = long;     /* char position in active file */
-using MemorySize = unsigned int; /* for passing to malloc */
-using Uchar = unsigned char;     /* more space-efficient */
+using NewsgroupNum = int;        // newsgroup number
+using ArticleNum = long;         // article number
+using ArticleUnread = long;      // could be short to save space
+using ArticlePosition = long;    // char position in article file
+using ArticleLine = int;         // line position in article file
+using ActivePosition = long;     // char position in active file
+using MemorySize = unsigned int; // for passing to malloc
+using Uchar = unsigned char;     // more space-efficient
 using stat_t = struct stat;
 
 #endif
