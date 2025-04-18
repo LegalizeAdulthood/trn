994a33e7546fb8f685242cc632a24ba641908f40
Richard <legalize@xmission.com>
2025-04-18 14:41:35 -0700

    Use // style comments: color.h

--- libtrn/include/trn/color.h
+++ libtrn/include/trn/color.h
@@ -12,25 +12,25 @@
 enum ObjectNumber
 {
     COLOR_DEFAULT = 0,
-    COLOR_NG_NAME,    /* NG name in thread selector      */
-    COLOR_PLUS,       /* + in thread selector            */
-    COLOR_MINUS,      /* - in thread selector            */
-    COLOR_STAR,       /* * in thread selector            */
-    COLOR_HEADER,     /* headers in article display      */
-    COLOR_SUBJECT,    /* subject in article display      */
-    COLOR_TREE,       /* tree in article display         */
-    COLOR_TREE_MARK,  /* tree in article display, marked */
-    COLOR_MORE,       /* the more prompt                 */
-    COLOR_HEADING,    /* any heading                     */
-    COLOR_CMD,        /* the command prompt              */
-    COLOR_MOUSE,      /* the mouse bar                   */
-    COLOR_NOTICE,     /* notices, e.g. kill handling     */
-    COLOR_SCORE,      /* score objects                   */
-    COLOR_ART_LINE1,  /* the article heading             */
-    COLOR_MIME_SEP,   /* the multipart mime separator    */
-    COLOR_MIME_DESC,  /* the multipart mime description line */
-    COLOR_CITE_DTEXT, /* cited text color                */
-    COLOR_BODY_TEXT,  /* regular body text               */
+    COLOR_NG_NAME,    // NG name in thread selector
+    COLOR_PLUS,       // + in thread selector
+    COLOR_MINUS,      // - in thread selector
+    COLOR_STAR,       // * in thread selector
+    COLOR_HEADER,     // headers in article display
+    COLOR_SUBJECT,    // subject in article display
+    COLOR_TREE,       // tree in article display
+    COLOR_TREE_MARK,  // tree in article display, marked
+    COLOR_MORE,       // the more prompt
+    COLOR_HEADING,    // any heading
+    COLOR_CMD,        // the command prompt
+    COLOR_MOUSE,      // the mouse bar
+    COLOR_NOTICE,     // notices, e.g. kill handling
+    COLOR_SCORE,      // score objects
+    COLOR_ART_LINE1,  // the article heading
+    COLOR_MIME_SEP,   // the multipart mime separator
+    COLOR_MIME_DESC,  // the multipart mime description line
+    COLOR_CITE_DTEXT, // cited text color
+    COLOR_BODY_TEXT,  // regular body text
     MAX_COLORS
 };
 
