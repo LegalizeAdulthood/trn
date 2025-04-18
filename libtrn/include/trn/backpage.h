9b2294553b2e3aa371d23283244afc23bd34a62f
Richard <legalize@xmission.com>
2025-04-18 14:41:34 -0700

    Use // style comments: backpage.h

--- libtrn/include/trn/backpage.h
+++ libtrn/include/trn/backpage.h
@@ -1,12 +1,12 @@
 /* trn/backpage.h
  */
-/* This software is copyrighted as detailed in the LICENSE file. */
+// This software is copyrighted as detailed in the LICENSE file.
 #ifndef TRN_BACKPAGE_H
 #define TRN_BACKPAGE_H
 
 #include <config/typedef.h>
 
-/* things for doing the 'back page' command */
+// things for doing the 'back page' command
 
 void back_page_init();
 ArticlePosition virtual_read(ArticleLine index);
