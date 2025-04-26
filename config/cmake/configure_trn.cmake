1ef5c9b7a5b23ea546c88b27d9543b3b881afb66
Richard <legalize@xmission.com>
2025-04-26 13:22:51 -0600

    Configure SELECT_INIT

--- config/cmake/configure_trn.cmake
+++ config/cmake/configure_trn.cmake
@@ -109,6 +109,7 @@ function(configure_trn)
     set(OVERVIEW_DIR "" CACHE STRING "The directory containing overview files.")
     set(OVERVIEW_FMT "" CACHE STRING "The name of the overview.fmt file.")
     option(THREAD_INIT "If ON, indicates we act like trn no matter what our name is." OFF)
+    option(SELECT_INIT "If ON, indicates we default to the selector for group entry." OFF)
 
     if(WIN32)
         set(MSDOS ON)
